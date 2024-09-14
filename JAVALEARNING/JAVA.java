3107186a22de3225ab2cc75eb83f26d1c7698352
anurag <anuragshrivastav699@gmail.com>
2024-09-15 01:48:52 +0530

    starting to learn java with data structures and algorithms

---
+++ JAVALEARNING/JAVA.java
@@ -0,0 +1,8 @@
+package demo.JAVALEARNING;
+
+public class JAVA {
+    public static void main(String[] args) {
+        System.out.println("hello everyone my java learning starts here ");
+
+    }
+}
