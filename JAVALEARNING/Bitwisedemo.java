22bb0e8acbf8dd76159aa2a1e137638598625908
basedanurag <anuragsrivastava241529@gmail.com>
2024-11-07 02:02:14 +0530

    tested left,right shift prog

--- JAVALEARNING/Bitwisedemo.java
+++ JAVALEARNING/Bitwisedemo.java
@@ -3,9 +3,12 @@ package JAVALEARNING;
 public class Bitwisedemo {
     public static void main(String[] args) {
         int x=0b1010;
-        int y=0b0110,z;
+        int y;
+        /* int z;
         z=x^y; //use &,|,^,<<,>> operators for learning 
-        System.out.println(z);
+        System.out.println(z); */
+        y=x<<1;
+        System.out.println(y);
 
     }
     
