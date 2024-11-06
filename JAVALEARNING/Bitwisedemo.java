fa841e569f335a398e9493ae27bd0e62aec01079
basedanurag <anuragsrivastava241529@gmail.com>
2024-11-07 01:51:44 +0530

    changes

--- JAVALEARNING/Bitwisedemo.java
+++ JAVALEARNING/Bitwisedemo.java
@@ -4,7 +4,7 @@ public class Bitwisedemo {
     public static void main(String[] args) {
         int x=0b1010;
         int y=0b0110,z;
-        z=x|y; //use &,|,~ operators for learning 
+        z=x^y; //use &,|,^ operators for learning 
         System.out.println(z);
 
     }
