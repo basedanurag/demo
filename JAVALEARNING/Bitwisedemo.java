b9612da22346ed626f35daa94a5b20c415c97aa2
basedanurag <anuragsrivastava241529@gmail.com>
2024-11-07 02:32:25 +0530

    new question

--- JAVALEARNING/Bitwisedemo.java
+++ JAVALEARNING/Bitwisedemo.java
@@ -3,12 +3,12 @@ package JAVALEARNING;
 public class Bitwisedemo {
     public static void main(String[] args) {
         int x=0b1010;
-        int y;
+       
         /* int z;
         z=x^y; //use &,|,^,<<,>> operators for learning 
         System.out.println(z); */
-        y=x<<1;
-        System.out.println(y);
+        
+        System.out.println(x);
 
     }
     
