73db94ad7d077790213d87ae16a4954c2e57c400
basedanurag <anuragsrivastava241529@gmail.com>
2025-01-07 02:04:06 +0530

    ...

--- JAVALEARNING/Stringpractice.java
+++ JAVALEARNING/Stringpractice.java
@@ -1,6 +1,6 @@
 public class Stringpractice {
     public static void main(String[] args){
-        String str1 = "java program";
+       /*  String str1 = "java program";
         String str2 = new String("java");
         //byte string "String str3 = new String(b,1,2 );"this will store the string in heap memory and the numbers are indexes which is going to be stored not the full string "
         byte b[] =  {65,66,67,68,69};
@@ -11,7 +11,27 @@ public class Stringpractice {
         System.out.println(str1);
         System.out.println(str2);
         System.out.println(str3);
-        System.out.println(str4);
+        System.out.println(str4);*/
+
+
+
+        // another case 
+
+
+
+       /* String str1="Java";
+        String str2="Java";
+        
+        System.out.println(str1==str2); //here the code will show true due to java pool memory both reference are pointing toward  the same object */
+
+
+        // another case 
+
+
+        /* String str1="Java";
+           String str2="java";
+        String str2=new String("Java"); // here the code will show the output as false because of the new keyword the string is now saved in the heap memory
+        System.out.println(str1==str2); */
 
     }
     
