bbc0f496bfc586ed55001182352043c1cf9549b8
basedanurag <anuragsrivastava241529@gmail.com>
2025-01-12 01:09:21 +0530

    String methods and their uses

---
+++ JAVALEARNING/Stringmethods.java
@@ -0,0 +1,40 @@
+public class Stringmethods {
+    public static void main (String[] args )
+    {
+        String str = new String ("Anurag srivastava");
+     /* using .length();method
+            System.out.println(str.length()); 
+            or we can do this
+            int len = str.length();
+            System.out.println(len);
+            */
+     /* using .toUpperCase();
+      in this method the method will change the lower cases into upper cases of the String but it will not modify the String but it will give a new String
+      here if we donot take the new variable for new String and want to modify the same name to the previous String variable then it can be done the String will give the new address of the new String 
+      but the prev String is deleted or we can say it as it is a garbage collected String 
+      example:
+      String str2 = str.toUpperCase();
+      System.out.println(str2);
+       // similarly we can use the lower case method to convert all of them into a lowercase String 
+       example :
+       String str2 = str.toLowerCase();
+      System.out.println(str2);
+      */ 
+     /* using .trim();
+       * here the trim method will trim the String it will remove the empty spaces in the String 
+       * the trim method will only ewmove the spaces from the begining and  the end of the String 
+       * example : 
+       * String Str2 = str.trim();
+       System.out .println(Str2);
+
+       */ 
+     /* using .replace();       
+      * in this method we can replace the character from the String to another character 
+      example :
+       String str2  = str.replace('i','m');
+     System.out.println(str2 +"<-new String , old String -> " + str);
+     */
+    }
+
+    
+}
