fe34364554012cc45e56ff79f8e96855225db4ea
basedanurag <anuragsrivastava241529@gmail.com>
2025-01-22 02:09:37 +0530

    day21 updated

--- leetcode/Q2017.java
+++ leetcode/Q2017.java
@@ -1,4 +1,23 @@
 public class Q2017 {
+  
+        public long gridGame(int[][] grid) {
+            long row1sum =0;
+            for (int num : grid[0]) {
+                row1sum += num;  
+            }
+            long row2remainSum = 0;
+            long minimizedRobo2Sum = Long.MAX_VALUE;
+            for(int Robot1col =  0; Robot1col < grid[0].length; Robot1col++){
+                row1sum -= grid[0][Robot1col];
+                
+                long bestOfRobot2 = Math.max(row1sum , row2remainSum);
+    
+                minimizedRobo2Sum = Math.min(minimizedRobo2Sum, bestOfRobot2);
+                
+                row2remainSum += grid[1][Robot1col];
+             }
+            return minimizedRobo2Sum;
+           }
+}
 
     
-}
