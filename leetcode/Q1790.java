49dbec89cba52c648ff49c99cca97bc9112addca
basedanurag <anuragsrivastava241529@gmail.com>
2025-02-05 05:50:40 +0530

    36

---
+++ leetcode/Q1790.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q1790 {
+  
+    public boolean areAlmostEqual(String s1, String s2) {    
+        if( s1.equals(s2)) {
+            return true; 
+        } 
+        List<Integer> diffIndices = new ArrayList<>(); 
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                diffIndices.add(i);
+            }
+        }
+        
+        if (diffIndices.size() != 2) {
+            return false;
+        }
+        
+        int i = diffIndices.get(0);
+        int j = diffIndices.get(1);
+        return s1.charAt(i) == s2.charAt(j) && s1.charAt(j) == s2.charAt(i);
+    }
+}
+
--- leetcode/Q900.java
+++
@@ -1,5 +0,0 @@
-public class Q900 {
-    // this is a test question to maintain the github streak 
-    // this question does not exist 
-    // change the question number tomorrow 
-}
