702ec576a6382ee4065c22542104b8a620b37cac
basedanurag <anuragsrivastava241529@gmail.com>
2025-02-09 20:01:44 +0530

    40

---
+++ leetcode/Q2364.java
@@ -0,0 +1,20 @@
+import java.util.Map;
+
+public class Q2364 {
+        public long countBadPairs(int[] nums) {
+            int n = nums.length;
+            long result = 0;
+            Map<Integer, Integer> mp = new HashMap<>();
+            for (int i =0; i< n ;i++) {
+                int diff = nums[i]-i;
+                int totalPairs =i;
+                int goodpairs = mp.getOrDefault(diff, 0);
+                result += (totalPairs - goodpairs);
+                mp.put(diff,goodpairs +1);
+    
+    
+            }
+            
+            return result;
+        }
+}
