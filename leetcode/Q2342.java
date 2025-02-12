8d79a9ef6be8b05325839a1f64b73e909eac3d3a
basedanurag <anuragsrivastava241529@gmail.com>
2025-02-12 14:07:34 +0530

    43 updated

--- leetcode/Q2342.java
+++ leetcode/Q2342.java
@@ -1,3 +1,27 @@
 public class Q2342 {
+  
+        private int getDigitSum(int num) {
+            int sum = 0;
+            while(num>0) {
+                sum += (num % 10);
+                num /= 10;            
+            }
+            return sum;
+        }
+        public int maximumSum(int[] nums) {
+            int n = nums.length;
+            int result = -1;
+            int[] mp = new int[100];
+            for(int i=0; i< n ; i++) {
+                int digitSum = getDigitSum(nums[i]);
+                if(mp[digitSum]>0) {
+                    result = Math.max(result, nums[i] +  mp[digitSum]);
+                }
+                mp[digitSum] =  Math.max(mp[digitSum], nums[i]);
     
+            }
+    
+            return result;
+        }
+   
 }
