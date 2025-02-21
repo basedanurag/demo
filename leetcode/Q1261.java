180f537746e414a0b83eed7bc5d73d84da1343d2
basedanurag <anuragsrivastava241529@gmail.com>
2025-02-22 00:40:30 +0530

    52

--- leetcode/Q1261.java
+++ leetcode/Q1261.java
@@ -1,3 +1,29 @@
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.swing.tree.TreeNode;
+
 public class Q1261 {
-    
+    class FindElements {
+    Set<Integer> st = new HashSet<>();
+
+    private void dfs(TreeNode root, int x) {
+        if (root == null)
+            return;
+
+        root.val = x;
+        st.add(x);
+        dfs(root.left, 2 * x + 1);
+        dfs(root.right, 2 * x + 2);
+    }
+
+    public FindElements(TreeNode root) {
+        st.clear();
+        dfs(root, 0);
+    }
+
+    public boolean find(int target) {
+        return st.contains(target);
+    }
+}
 }
