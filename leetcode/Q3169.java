f6897aec89b6df4941afad2bf0c5940f6284157a
basedanurag <anuragsrivastava241529@gmail.com>
2025-03-25 02:11:26 +0530

    83 updated

--- leetcode/Q3169.java
+++ leetcode/Q3169.java
@@ -1,3 +1,26 @@
+import java.util.Arrays;
+
 public class Q3169 {
-    
+   class Solution { 
+    public int countDays(int days, int[][] meetings) {
+        Arrays.sort(meetings, (a,b) -> Integer.compare(a[0],b[0]));
+
+        int result =0;
+        int end = 0;
+
+        for(int[] meet : meetings) {
+            if(meet[0] > end) {
+                result += meet[0] - end -1;
+            }
+            end = Math.max(end, meet[1]);
+
+        }
+        if(days> end){
+            result += days- end;
+
+        }
+        return result;
+        
+    }
+} 
 }
