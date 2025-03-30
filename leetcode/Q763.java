a455806ce51f565f3d5701dc2827095d913fc313
basedanurag <anuragsrivastava241529@gmail.com>
2025-03-31 02:51:00 +0530

    89

---
+++ leetcode/Q763.java
@@ -0,0 +1,27 @@
+public class Q763 {
+    class Solution {
+        public List<Integer> partitionLabels(String s) {
+        int n  = s.length();
+        List<Integer> result = new ArrayList<>();
+        // last ouucurance of each character
+        int[]  lastIndex = new int[26];
+        for(int i = 0; i < n; i++){
+            lastIndex[s.charAt(i) - 'a'] = i;
+    
+        }
+        int i =0;
+        int start = 0;
+        int end = 0;
+        while(i<n) {
+            end = Math.max(end,lastIndex[s.charAt(i)-'a']);
+            if(i == end) {
+                result.add(end - start +1);
+                start = end + 1;
+            }
+            i++;
+        }
+        return result;
+        
+        }
+    }
+}
