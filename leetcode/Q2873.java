e7ce6dc8af4e1c3c48c38905efa5bd6af3cacb38
basedanurag <anuragsrivastava241529@gmail.com>
2025-04-02 23:15:53 +0530

    92

---
+++ leetcode/Q2873.java
@@ -0,0 +1,16 @@
+public class Q2873 {
+    class Solution {
+        public long maximumTripletValue(int[] nums) {
+            int n = nums.length;
+             long result = 0;
+            for(int i =0; i < n ; i++) {
+                for(int j = i+1; j<n; j++){
+                    for(int k = j+1; k < n; k++){
+                        result  =  Math.max(result, (long)(nums[i] - nums[j])*nums[k]);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
