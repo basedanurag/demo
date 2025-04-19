e9405de63c3ed21a80b6bb99aa84213c4d068721
basedanurag <anuragsrivastava241529@gmail.com>
2025-04-20 01:32:46 +0530

    109

---
+++ leetcode/Q2563.java
@@ -0,0 +1,30 @@
+public class Q2563 {
+    class Solution {
+        public long lowerBound(int[] nums, int low, int high, int element) {
+            while(low<=high) {
+                int mid= low +((high -low)/2);
+                if(nums[mid] >= element){
+                    high = mid-1;
+    
+                }
+                else {
+                    low = mid +1;
+                }
+            }
+            return low;
+        }
+        public long countFairPairs(int[] nums, int lower, int upper) {
+            Arrays.sort(nums);
+            long ans =0;
+            for(int i = 0; i< nums.length; i++){
+                long low = lowerBound(nums,i+1,nums.length - 1,lower - nums[i]);
+    
+                // high pairs sum <= upper
+                long high = lowerBound(nums, i+1, nums.length -1,upper - nums[i] +1);
+    
+                ans += 1 * (high - low);
+            }
+            return ans;
+        }
+    }
+}
