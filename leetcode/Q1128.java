f518042e49634cc77ad3a5d68b2414b9e6d93222
basedanurag <anuragsrivastava241529@gmail.com>
2025-05-05 00:00:37 +0530

    124 full code

--- leetcode/Q1128.java
+++ leetcode/Q1128.java
@@ -1,3 +1,19 @@
 public class Q1128 {
+    class Solution {
+        public int numEquivDominoPairs(int[][] dominoes) {
+            int[] mp = new int[100];
+            for(int[] d : dominoes)
+                if(d[0]>d[1]) mp[d[0] * 10 + d[1]]++;
+                else mp[d[1]*10 + d[0]]++;
+            
+            int count = 0;
+            for(int freq : mp)
+            // combination formulla
+            count += (freq -1) * freq / 2;
     
+    
+            return count;
+    
+        }
+    }
 }
