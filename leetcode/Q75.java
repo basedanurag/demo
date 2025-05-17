9f52cde6f0224be5eb7b078561a96e3e0c908351
basedanurag <anuragsrivastava241529@gmail.com>
2025-05-18 04:11:04 +0530

    137

--- leetcode/Q75.java
+++ leetcode/Q75.java
@@ -1,3 +1,21 @@
 public class Q75 {
-    
+    class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int left = 0, mid = 0, right = n - 1;
+        while(mid <= right){
+            if(nums[mid] == 0){
+                int temp = nums[left];
+                nums[left++] = nums[mid];
+                nums[mid++] = temp;
+            }else if(nums[mid] == 1){
+                mid++;
+            }else {
+                int temp = nums[mid];
+                nums[mid] = nums[right];
+                nums[right--] = temp;
+            }
+        }
+    }
+}
 }
