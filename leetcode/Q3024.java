f1da8c0b5fe2ef0bf871b1f01b548d1ebc064b56
basedanurag <anuragsrivastava241529@gmail.com>
2025-05-20 01:13:38 +0530

    139

---
+++ leetcode/Q3024.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Q3024 {
+    class Solution {
+    public String triangleType(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums)
+        set.add(num);
+        if(nums[0]+ nums[1] <= nums[2]) return "none";
+        if(set.size() == 1) return "equilateral";
+        if(set.size() == 2) return "isosceles";
+        return "scalene";
+    }
+}
+}
