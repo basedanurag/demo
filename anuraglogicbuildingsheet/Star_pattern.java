85bc7aaa4930b6c56ff52689cda0a72a71189494
basedanurag <anuragsrivastava241529@gmail.com>
2025-06-05 01:41:00 +0530

    155

--- anuraglogicbuildingsheet/Star_pattern.java
+++ anuraglogicbuildingsheet/Star_pattern.java
@@ -11,8 +11,6 @@ class Star_pattern{
             System.out.println(" ");
 
         }*/
-
-
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= n - i + 1; j++){
                 System.out.print("*");
