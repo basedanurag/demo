df9002aeaf9ce517061f75343cf59c89f8f5ef16
basedanurag <anuragsrivastava241529@gmail.com>
2025-06-04 09:38:01 +0530

    pattern practice logic building sheet day 1

--- JAVALEARNING/Pattern.java
+++ JAVALEARNING/Pattern.java
@@ -1,9 +1,9 @@
 public class Pattern {
     public static void main(String[] args){
-        int cnt =0;
-        for(int i  = 5; i >=1; i--){
+       
+        for(int i = 10; i >=1; i--){
             for(int j  = 1; j<= i; j++){
-             ++cnt;
+            
                 System.out.print(j+ " " );
                
             } 
