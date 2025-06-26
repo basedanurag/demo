d9d83db16150756c79b13c09618671aee07938a6
basedanurag <anuragsrivastava241529@gmail.com>
2025-06-27 03:03:29 +0530

    177

---
+++ leetcode/Q2133.java
@@ -0,0 +1,23 @@
+public class Q2133 {
+    class Solution {
+    public int longestSubsequence(String s, int k) {
+        int length = 0;
+        long powerValue = 1;
+
+        for (int i = s.length() - 1; i >= 0; --i) {
+            if (s.charAt(i) == '0') {
+                length++;
+            } else if (powerValue <= k) {
+                length++;
+                k -= powerValue;
+            }
+
+            if (powerValue <= k) {
+                powerValue <<= 1; // powerValue *= 2
+            }
+        }
+
+        return length;
+    }
+}
+}
