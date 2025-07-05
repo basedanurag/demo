5e6d075af7cab2fb69fe23e7e6d6a4262c42c3e4
basedanurag <anuragsrivastava241529@gmail.com>
2025-07-05 23:43:52 +0530

    186

--- leetcode/Q1394.java
+++ leetcode/Q1394.java
@@ -1,3 +1,22 @@
+import java.util.HashMap;
+
 public class Q1394 {
-    
+    class Solution {
+    public int findLucky(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int n : arr) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        int luckyInteger = -1;
+
+        for (int key : map.keySet()) {
+            if (map.get(key) == key) {
+                luckyInteger = key; 
+            }
+        }
+        return luckyInteger;  
+    }
+}
 }
