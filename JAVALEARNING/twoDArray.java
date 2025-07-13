1b169971f03a39bba4f4c8b563ce1b95784ff92b
basedanurag <anuragsrivastava241529@gmail.com>
2025-07-14 00:04:05 +0530

    learned the matrix m,ultiplication program

--- JAVALEARNING/twoDArray.java
+++ JAVALEARNING/twoDArray.java
@@ -1,6 +1,6 @@
 public class twodArray {
     public static void main(String[] args) {
-        int arr[][]  = {
+       /*  int arr[][]  = {
         {1,2,3,4,5,6,7,8,9,10},{1,2,3,4,5,6,7,8,9,10},
         {1,2,3,4,5,6,7,8,9,10},{1,2,3,4,5,6,7,8,9,10},
         {1,2,3,4,5,6,7,8,9,10},{1,2,3,4,5,6,7,8,9,10},
@@ -12,5 +12,34 @@ public class twodArray {
             }
             System.out.println("\n");
          }
+    */
+    int[][] arr = new int[3][3];
+       matrixMult(arr);
+    
+    }
+    static void matrixMult(int[][] arr){
+        int mat1[][] = {{1,2,3}, {4,5,6},{7,8,9}};
+        int mat2[][] = {{1,2,3}, {4,5,6},{7,8,9}};
+        if(mat1[0].length != mat2.length){
+            System.out.println("Invalid ");
+            return;
+        }
+        else {
+          for(int i = 0; i < mat1.length; i++) {
+            for(int  j = 0; j < mat2.length; j++){
+                arr[i][j] = 0;
+                for(int k = 0; k < 3; k++){
+                   arr[i][j] += mat1[i][k] * mat2[k][j];
+                } 
+            }
+          }  
+          for(int x[] :arr){
+          for(int val : x){
+            System.out.print(val + " ");
+          
+          }
+            System.out.println("");
+        }
+        }
     }
 }
