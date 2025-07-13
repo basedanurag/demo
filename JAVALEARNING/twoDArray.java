3c7f6171ff64a37d040b1b8acc55bc5fb663da09
basedanurag <anuragsrivastava241529@gmail.com>
2025-07-13 23:34:39 +0530

    2d arr

--- JAVALEARNING/twoDArray.java
+++ JAVALEARNING/twoDArray.java
@@ -1,4 +1,4 @@
-public class twoDArray {
+public class twodArray {
     public static void main(String[] args) {
         int arr[][]  = {
         {1,2,3,4,5,6,7,8,9,10},{1,2,3,4,5,6,7,8,9,10},
@@ -8,7 +8,7 @@ public class twoDArray {
         {1,2,3,4,5,6,7,8,9,10},{1,2,3,4,5,6,7,8,9,10}};
          for(int i = 0; i < arr.length;i++){
             for(int j = 0; j < arr[0].length; j++){
-                System.out.println(arr[i][j]);
+                System.out.print(arr[i][j]);
             }
             System.out.println("\n");
          }
