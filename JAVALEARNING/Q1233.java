d0c96681464e35ca7228e76abe0df70d5d40d3c6
basedanurag <anuragsrivastava241529@gmail.com>
2025-07-20 05:18:13 +0530

    200

---
+++ JAVALEARNING/Q1233.java
@@ -0,0 +1,20 @@
+public class Q1233 {
+    class Solution {
+    public List<String> removeSubfolders(String[] folder) {
+        Arrays.sort(folder);
+        List<String> result = new ArrayList<>();
+        result.add(folder[0]); // the first folder can newver be the sub folder 
+
+        for(int i = 1; i < folder.length; i++){
+            String currFolder = folder[i];
+            String lastFolder = result.get(result.size() - 1);
+            lastFolder += "/";
+
+            if(!currFolder.startsWith(lastFolder)){
+                result.add(currFolder);
+            }
+        }
+        return result;
+    }
+}
+}
