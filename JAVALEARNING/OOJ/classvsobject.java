b3d3c1a44e4590676a84c5426324590df628fa79
basedanurag <anuragsrivastava241529@gmail.com>
2025-07-30 03:02:18 +0530

    hehe

---
+++ JAVALEARNING/OOJ/classvsobject.java
@@ -0,0 +1,18 @@
+package OOJ;
+class classvsobject {
+    private int channel;
+    private int volume;
+    public void changeChannel(){
+            // try code
+    }
+    public void changeVolume(){
+        // try code 
+    }
+}
+class Test{
+    public static void main(String[] args) {
+        classvsobject t = new classvsobject();
+        t.changeChannel();
+        t.changeVolume();
+    }
+}
