74e2dee86b41ac040d288b5c5b5b0aecaed15d42
basedanurag <anuragsrivastava241529@gmail.com>
2025-08-02 06:33:08 +0530

    updates

--- JAVALEARNING/Shapes.java
+++ JAVALEARNING/Shapes.java
@@ -1,7 +1,20 @@
 //rectangle class
 class Rectangle {
-    public double length;
-    public double breadth;
+    private double length;
+    private double breadth;
+    public double getLength(){
+        return length;
+    }
+    public double getBreadth(){
+        return breadth;
+    }
+    public void setLength(double l){
+        length = l;
+    }
+    public void setBreadth(double b){
+        breadth = b;
+    }
+
     public double area(){
         return length * breadth;
     }
@@ -37,16 +50,18 @@ public class Shapes {
     public static void main(String[] args){
         Cylinder c = new Cylinder();
        Rectangle R1 = new Rectangle();
-       R1.length = 5;
-       R1.breadth = 6;
+       R1.setLength(5);
+       R1.setBreadth(5);
        //radius and height of the cylinder 
        c.radius = 7;
        c.height = 10;
+       System.out.println("printing for rectangle");
        System.out.println("Area " + R1.area());
        System.out.println("Perimeter " + R1.perimeter());
        System.out.println("Square " + R1.isSquare());
 
-
+          
+       System.out.println("        \n printing for cylinder     ");
        System.out.println("lid Area " + c.lidArea());
        System.out.println("total Surface Area " + c.totalSurfaceArea());
        System.out.println("circumeference " + c.circumference());
