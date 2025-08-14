9c390d76d18577f697ce39742bf6ff0d93ac9c24
basedanurag <anuragsrivastava241529@gmail.com>
2025-08-14 23:20:13 +0530

    17 pattern done of striver sheet

--- A2Z sheet solutions/Patterns.java
+++ A2Z sheet solutions/Patterns.java
@@ -1,213 +1,304 @@
 //import java.util.*;
 
 public class Patterns {
-public static void p1(int n){
+    public static void p1(int n) {
         /*
-* * * * *
-* * * * *
-* * * * *
-* * * * *
-* * * * *
- */
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-        System.out.print("*");
-        }
+         * * * * *
+         * * * * *
+         * * * * *
+         * * * * *
+         * * * * *
+         */
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
 
-    System.out.println();
-    }
-}
-public static void p2(int n){
-    /*
-* 
-* * 
-* * *
-* * * *
-* * * * *
-* * * * * *
-     */
-    for(int i = 0; i <= n; i++){
-        for(int j  = 0; j < i; j++){
-            System.out.print("*");
+            System.out.println();
         }
-        System.out.println();
-    }
-}
-public static void p3(int n ){
-    
-    for(int i = 0; i <= n; i++){
-           for(int j  = 0; j < i; j++){
-            System.out.print(j + 1 );
-        }
-        System.out.println();
-    }
-}
-public static void p4(int n){
-     for(int i = 0; i <= n; i++){
-           for(int j  = 0; j <= i; j++){
-            System.out.print(i + 1 );
-        }
-        System.out.println();
     }
 
-}
-public static void p5(int n){
-    for( int i  = 0; i < n; i++){
-        for(int j  =  n ; j > i; j--){
-            System.out.print("*");
+    public static void p2(int n) {
+        /*
+         * 
+         * *
+         * * *
+         * * * *
+         * * * * *
+         * * * * * *
+         */
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        System.out.println();
-    }
-}
-public static void p6(int n){
-    for(int i = 0; i <= n; i++){
-        for(int j = n; j > i ; j--){
-            System.out.print(n - j + 1);
-        }
-        System.out.println();
-    }
-}
-public static void p7(int n){
-    for(int i = 0 ; i < n ; i++){
-        for(int j  = 0; j < n - i - 1; j++){
-            System.out.print( " ");
-        }
-        for(int s = 0; s < (2 * i + 1); s++){
-            System.out.print("*");
-        }
-        for(int k = 0; k <n -i - 1; k++){
-            System.out.print(" ");
-        }
-        System.out.println();
-    }
-}
-public static void p8(int n ){
-    for(int i = 0 ; i < n ; i++){
-        //space
-        for(int j  = 0
-        ; j <  i; j++){
-            System.out.print( " ");
-        }//stars 
-        for(int s = 0; s < (2 *n) - (2 *i+ 1); s++){
-            System.out.print("*");
-        }
-        // space 
-        for(int k = 0; k <i ; k++){
-            System.out.print(" ");
-        }
-        System.out.println();
-    }
-}
-public static void p9(int n){
-     for(int i = 0 ; i < n ; i++){
-        for(int j  = 0; j < n - i - 1; j++){
-            System.out.print( " ");
-        }
-        for(int s = 0; s < (2 * i + 1); s++){
-            System.out.print("*");
-        }
-        for(int k = 0; k <n -i - 1; k++){
-            System.out.print(" ");
-        }
-        
-        System.out.println();
-    }
-    for(int i = 0 ; i < n ; i++){
-        //space
-        for(int j  = 0
-        ; j <  i; j++){
-            System.out.print( " ");
-        }//stars 
-        for(int s = 0; s < (2 *n) - (2 *i+ 1); s++){
-            System.out.print("*");
-        }
-        // space 
-        for(int k = 0; k < i ; k++){
-            System.out.print(" ");
-        }
-        System.out.println();
     }
 
-}
-public static void p10(int n ){
-      // Outer loop for number of rows.
-    for(int i=1;i<=2*n-1;i++){
-        
-          // stars would be equal to the row no. uptill first half
-          int stars = i;
-          
-          // for the second half of the rotated triangle.
-          if(i>n) stars = 2*n-i;
-          
-          // for printing the stars in each row.
-          for(int j=1;j<=stars;j++){
-              System.out.print("*");
-          }
-          
-          // As soon as the stars for each iteration are printed, we move to the
-          // next row and give a line break otherwise all stars
-          // would get printed in 1 line.
-          System.out.println();
+    public static void p3(int n) {
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(j + 1);
+            }
+            System.out.println();
+        }
     }
-}
-public static void p11(int n){
-    for(int i  =0 ; i < n; i++){
-        for(int j = 0; j <= i;j++){
-            if((i + j) % 2 == 0)
-             System.out.print(" 1 ");
-             else
-             System.out.print(" 0 ");
+
+    public static void p4(int n) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(i + 1);
+            }
+            System.out.println();
         }
-        System.out.println();
+
     }
-}
-public static void p12(int n){
-    int space = 2 *(n - 1);
 
-    for(int i = 1; i  < n; i++){
-        
-        for(int j = 1; j <= i; j++){
-            System.out.print(j);
+    public static void p5(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        for(int s = 0; s <= space ; s++){
-            System.out.print(" ");
-        }
-        for(int k = i; k >= 1; k--){
-
-            System.out.print(k);
-        }
-        System.out.println();
-
-        space -= 2;
     }
-}
 
-    public static void main(String[] args){
-       System.out.println("pattern 1");
+    public static void p6(int n) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print(n - j + 1);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void p7(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int s = 0; s < (2 * i + 1); s++) {
+                System.out.print("*");
+            }
+            for (int k = 0; k < n - i - 1; k++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void p8(int n) {
+        for (int i = 0; i < n; i++) {
+            // space
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            } // stars
+            for (int s = 0; s < (2 * n) - (2 * i + 1); s++) {
+                System.out.print("*");
+            }
+            // space
+            for (int k = 0; k < i; k++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void p9(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int s = 0; s < (2 * i + 1); s++) {
+                System.out.print("*");
+            }
+            for (int k = 0; k < n - i - 1; k++) {
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            // space
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            } // stars
+            for (int s = 0; s < (2 * n) - (2 * i + 1); s++) {
+                System.out.print("*");
+            }
+            // space
+            for (int k = 0; k < i; k++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void p10(int n) {
+        // Outer loop for number of rows.
+        for (int i = 1; i <= 2 * n - 1; i++) {
+
+            // stars would be equal to the row no. uptill first half
+            int stars = i;
+
+            // for the second half of the rotated triangle.
+            if (i > n)
+                stars = 2 * n - i;
+
+            // for printing the stars in each row.
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+
+            // As soon as the stars for each iteration are printed, we move to the
+            // next row and give a line break otherwise all stars
+            // would get printed in 1 line.
+            System.out.println();
+        }
+    }
+
+    public static void p11(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if ((i + j) % 2 == 0)
+                    System.out.print(" 1 ");
+                else
+                    System.out.print(" 0 ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void p12(int n) {
+        int space = 2 * (n - 1);
+
+        for (int i = 1; i < n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int s = 1; s <= space; s++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k >= 1; k--) {
+
+                System.out.print(k);
+            }
+            System.out.println();
+
+            space -= 2;
+        }
+    }
+
+    public static void p13(int n) {
+        int count = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(count + " ");
+                count += 1;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void p14(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            for (char ch = 'A'; ch <= 'A' + i; ch++) {
+                System.out.print(ch + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void p15(int n) {
+        for (int i = 0; i < n; i++) {
+
+            for (char ch = 'A'; ch <= 'A' + (n - i - 1); ch++) {
+
+                System.out.print(ch + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void p16(int n) {
+        char c = 'A';
+        for (int i = 0; i <= n; i++) {
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(c + " ");
+
+            }
+            c++;
+            System.out.println();
+        }
+    }
+
+    public static void p17(int n) {
+        for (int i = 0; i < n; i++) {
+            char c = 'A';
+            int breakPoint = (2 * i + 1) / 2;
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int s = 0; s < (2 * i + 1); s++) {
+                if (s < breakPoint) {
+
+                    System.out.print(c);
+                    c++;
+                } else {
+                    System.out.print(c);
+                    c--;
+                }
+            }
+            for (int k = 0; k < n - i - 1; k++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("pattern 1");
         p1(5);
-         System.out.println("pattern 2");
+        System.out.println("pattern 2");
         p2(5);
-         System.out.println("pattern 3");
+        System.out.println("pattern 3");
         p3(5);
-         System.out.println("pattern 4");
+        System.out.println("pattern 4");
         p4(5);
-         System.out.println("pattern 5");
+        System.out.println("pattern 5");
         p5(5);
-         System.out.println("pattern 6");
+        System.out.println("pattern 6");
         p6(5);
-         System.out.println("pattern 7");
-         p7(5);
-         System.out.println("pattern 8");
-         p8(5);
-         System.out.println("pattern 9");
-         p9(5);
-         System.out.println("pattern 10");
-         p10(5);
-         System.out.println("pattern 11");
-         p11(5);
-         System.out.println("pattern 12");
-         p12(5);
-
+        System.out.println("pattern 7");
+        p7(5);
+        System.out.println("pattern 8");
+        p8(5);
+        System.out.println("pattern 9");
+        p9(5);
+        System.out.println("pattern 10");
+        p10(5);
+        System.out.println("pattern 11");
+        p11(5);
+        System.out.println("pattern 12");
+        p12(5);
+        System.out.println("pattern 13");
+        p13(5);
+        System.out.println("pattern 14");
+        p14(5);
+        System.out.println("pattern 15");
+        p15(5);
+        System.out.println("pattern 16");
+        p16(5);
+        System.out.println("pattern 17");
+        p17(5);
 
     }
 }
