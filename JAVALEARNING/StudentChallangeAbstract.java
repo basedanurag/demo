42d2c2ff0bd632f698744919f2964d08d36ad0f7
basedanurag <anuragsrivastava241529@gmail.com>
2025-08-21 01:30:32 +0530

    abstract classes end

---
+++ JAVALEARNING/StudentChallangeAbstract.java
@@ -0,0 +1,34 @@
+abstract class Shape{
+   public Shape(){System.out.println("shapes constructor");}
+    abstract void parameter();
+    abstract void area();
+}
+class circle extends Shape{
+    public void parameter(){
+        System.out.println("parameter of circle");
+    }
+    public void area(){
+        System.out.println("area of circle");
+    }
+
+}
+class Rectangle extends Shape{
+    public void parameter(){
+        System.out.println("parameter of Rectangle");
+    }
+    public void area(){
+        System.out.println("area of Rectangle");
+    }
+
+}
+
+public class StudentChallangeAbstract {
+    public static void main(String[] args) {
+        Shape s = new Rectangle();
+        Shape s2 = new circle();
+        s.area();
+        s.parameter();
+        s2.area();
+        s2.parameter();
+    }
+}
