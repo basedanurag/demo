9ad7e3a6726d95ddff8d00d7026f832f8c8c042e
basedanurag <anuragsrivastava241529@gmail.com>
2025-08-24 16:45:45 +0530

    palindrome qn done

--- A2Z sheet solutions/MathPlay.java
+++ A2Z sheet solutions/MathPlay.java
@@ -1,5 +1,30 @@
 public class MathPlay {
-   
+    public boolean isPalindrome(int x) {
+        String s = new String();
+        s = Integer.toString(x);
+        int n = s.length();
+        if (n == 1){
+            return true;
+        }
+            for(int i = 0 ; i <= n/2; i++){
+                if (s.charAt(i) != s.charAt(n - i - 1)){
+                    return false;
+                }
+            }
+        return true;
+    }
+    public boolean isPalindrome2(int x) {
+        if(x < 0 || ( x % 10 == 0 && x != 0))
+         return false;
+      int rev =  0;
+      int num = x;
+      while( num != 0){
+        int last = num % 10;
+        rev = (rev * 10 ) + last;
+            num = num/10;
+        }
+            return (rev == x);
+    } 
     public int countDigits1(int n) {
         int count = 0;
         while (n > 0) {
@@ -32,13 +57,18 @@ public class MathPlay {
     }
 
     public static void main(String[] args) {
-        int n = 12345;
+        int n = 123454321;
+        int x = 1000000001;
+
         MathPlay m = new MathPlay();
         
         // Method 1
         m.countDigits1(n);
-        
+        System.out.println(m.reverse(x));
         // Method 2
         System.out.println("Total digits (log method): " + m.countDigits2(n));
+        System.out.println(m.isPalindrome(n));
+        System.out.println(m.isPalindrome2(x));
+        
     }
 }
