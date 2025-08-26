218fa0edbfbdeda56f84085ca930dbac6f9f3288
basedanurag <anuragsrivastava241529@gmail.com>
2025-08-26 13:14:48 +0530

    student challange completed static and final

---
+++ JAVALEARNING/StudentChallangeStaticFinal.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Student{
+    private String rollNO ;
+    private static int count = 1;
+    public String generateROll(){
+        String rn = "UNIV_"  + count;
+        count++;
+        return rn;
+    }
+    //constructor
+    Student(){
+    rollNO = generateROll();
+    }
+    public String getRoll(){
+        return rollNO;
+    }
+
+}
+public class StudentChallangeStaticFinal{
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.generateROll();
+        System.out.println(s.getRoll());
+    }
+}
