9db8c44189cdf143fec907ab7a666f15c594dd22
basedanurag <anuragsrivastava241529@gmail.com>
2025-08-27 16:48:03 +0530

    huhh brain dead

--- JAVALEARNING/packageslearning/Test.java
+++
@@ -1,9 +0,0 @@
-import mypack1.Demo;
-
-public class Test {
-    public static void main(String[] args) {
-        Demo d1 = new Demo();
-        d1.display();
-      //  System.out.println(d1.a + d1.x + d1.y + d1.z);
-    }
-}
