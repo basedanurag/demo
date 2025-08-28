d5622e8d45ce84958466888175adce8b4a44c700
basedanurag <anuragsrivastava241529@gmail.com>
2025-08-28 14:07:10 +0530

    package inheritance completed

---
+++ JAVALEARNING/packageslearning/ChildClass.java
@@ -0,0 +1,9 @@
+
+import mypack2.newPack.TryThis.ParentClass;
+
+public class ChildClass extends ParentClass {
+    public static void main(String[] args) {
+        ChildClass obj = new ChildClass();
+        obj.greet(); // inherited method
+    }
+}
---
+++ JAVALEARNING/packageslearning/mypack2/newPack/TryThis/ParentClass.java
@@ -0,0 +1,7 @@
+package mypack2.newPack.TryThis;
+
+public class ParentClass {
+    protected void greet() {
+        System.out.println("Hello from ParentClass (other package)");
+    }
+}
