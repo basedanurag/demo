f550660c3142ff81663c531cf2b9c69b2231317b
basedanurag <anuragsrivastava241529@gmail.com>
2025-09-08 04:12:34 +0530

    recursion

--- A2Z sheet solutions/ReccursionPlayList.java
+++ A2Z sheet solutions/ReccursionPlayList.java
@@ -23,7 +23,7 @@ public class ReccursionPlayList {
     }
     public static void Linear3(int i,int n){ // using backtracking
         if (i < 1) // BASE CASE FOR LIMITING THE RECURSION
-        return;
+        return; 
         Linear3(i - 1, n);
         System.out.print(i + " ");
     }
@@ -33,11 +33,45 @@ public class ReccursionPlayList {
         Linear4(i + 1, n);
         System.out.print(i + " ");
     }
+    public int Sum(int i, int n ){
+        int sum = 0;
+        
+        if ( i < 1)
+        return 0;
+
+        sum  += i +  Sum(i - 1, n + i);
+        return sum;
+    }
+    public int fact(int n){
+        if ( n == 0)
+        return 1;
+        if (n == 1)
+        return 1;
+        
+       return   n * fact(n - 1 );
+
+    }
+      static void printArray(int arr[], int n) {
+      System.out.print("Reversed array is:- \n");
+      for (int i = 0; i < n; i++) {
+         System.out.print(arr[i] + " ");
+      }
+   }
+   //Function to reverse array using recursion
+   static void reverseArray(int arr[], int start, int end) {
+      if (start < end) {
+         int tmp = arr[start];
+         arr[start] = arr[end];
+         arr[end] = tmp;
+         reverseArray(arr, start + 1, end - 1);
+      }
+   }
 
 
 
 
     public static void main(String[] args){
+        ReccursionPlayList r = new ReccursionPlayList();
         Scanner sc = new Scanner(System.in);
         System.out.println("hello");
         int  n;
@@ -51,6 +85,13 @@ public class ReccursionPlayList {
         Linear3(n,n);
         System.out.println("output for Liner using back tracking( n to 1)");
         Linear4(1,n);
+        System.out.println("output for Sum" + " "+ r.Sum(n, n));
+        System.out.println("output for factorial"+ " " +r.fact(n));
+        
+        // reverse array ke lie array bana rha hu
+        int[] arr =  {1, 2, 3,5,6,7,3,5,7,7,5,4};
+        reverseArray(arr, 0, n -1);
+        printArray(arr, n);
 
 
 
