ae5c02ee810630045a45edd0a0db6a0bfc4cec3c
basedanurag <anuragsrivastava241529@gmail.com>
2025-09-17 01:40:00 +0530

    MethodRefference

--- JAVALEARNING/LambdaExpression/intro.java
+++ JAVALEARNING/LambdaExpression/intro.java
@@ -1,16 +1,16 @@
 @FunctionalInterface 
-interface MyLambda {
+interface MyLambda0 {
     public void display();
 }
-class my implements MyLambda {
+class my implements MyLambda0 {
     public void display(){
         System.out.println("hello from inheritance ");
     }
 }
 public class intro{
     public static void main(String[] args) {
-        MyLambda m1 = new my(); // object for calling
-        MyLambda m = () -> {System.out.println("hello world from lambda expression");}; // this is the lambda expression
+        MyLambda0 m1 = new my(); // object for calling
+        MyLambda0 m = () -> {System.out.println("hello world from lambda expression");}; // this is the lambda expression
         m.display();
         m1.display();
     }
--- JAVALEARNING/LambdaExpression/parameterLambda.java
+++ JAVALEARNING/LambdaExpression/parameterLambda.java
@@ -1,17 +1,17 @@
 
 @FunctionalInterface 
-interface MyLambda {
+interface MyLambda2 {
     public void display(String name);
 }
-class my implements MyLambda {
+class my implements MyLambda2 {
     public void display(String name ){
         System.out.println("hello from inheritance ");
     }
 }
 public class parameterLambda {
     public static void main(String[] args) {
-        MyLambda m1 = new my(); // object for calling
-        MyLambda m = (s) -> {System.out.println(s);}; // this is the lambda expression overriding 
+        MyLambda2 m1 = new my(); // object for calling
+        MyLambda2 m = (s) -> {System.out.println(s);}; // this is the lambda expression overriding 
         m.display("hello from lambda expression ");
         m1.display("hello from method overriding expression ");
     }  
