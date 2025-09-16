0bfe2dec36ca64e048f52efbe022d8c9fb6cf587
basedanurag <anuragsrivastava241529@gmail.com>
2025-09-17 00:14:20 +0530

    parameterLambda done

---
+++ JAVALEARNING/LambdaExpression/parameterLambda.java
@@ -0,0 +1,21 @@
+
+@FunctionalInterface 
+interface MyLambda {
+    public void display(String name);
+}
+class my implements MyLambda {
+    public void display(String name ){
+        System.out.println("hello from inheritance ");
+    }
+}
+public class parameterLambda {
+    public static void main(String[] args) {
+        MyLambda m1 = new my(); // object for calling
+        MyLambda m = (s) -> {System.out.println(s);}; // this is the lambda expression overriding 
+        m.display("hello from lambda expression ");
+        m1.display("hello from method overriding expression ");
+    }
+
+
+    
+}
