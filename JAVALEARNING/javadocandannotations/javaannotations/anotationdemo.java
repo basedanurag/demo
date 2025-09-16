d791732c6fd96c4724afae8881d38ac17ad6bc3b
basedanurag <anuragsrivastava241529@gmail.com>
2025-09-16 10:24:04 +0530

    basic annotations

--- JAVALEARNING/javadocandannotations/javaannotations/anotationdemo.java
+++ JAVALEARNING/javadocandannotations/javaannotations/anotationdemo.java
@@ -1,3 +1,32 @@
+
+
+abstract class My {
+    abstract public void display();
+    abstract public void show();
+}
+class  B extends My{
+     // there are two types of annotation 
+    //user defined pre built and user defined
+    @Override // this will give error if the method is not overridden 
+    public void display(){
+        System.out.println("hello");
+    }
+    @SuppressWarnings("Deprecated")
+    @Deprecated
+    public void show(){
+        System.out.println("hii");
+    }
+}
+
 public class anotationdemo {
+   
     
+    public static void main(String[] args) {
+        
+    
+       B b = new B();
+       
+        b.show();
+    }
+
 }
