bbb139e60e35e09e63bda94c04d4b7a3233ddc61
basedanurag <anuragsrivastava241529@gmail.com>
2025-09-19 02:10:34 +0530

    fileoutput streams done

--- JAVALEARNING/java io Streams/JavaStreams.java
+++ JAVALEARNING/java io Streams/JavaStreams.java
@@ -2,7 +2,13 @@ import java.io.*;
 public class JavaStreams {
     public static void main(String[] args) {
         try {
-             FileOutputStream fos = new FileOutputStream("Test.txt");
+             FileOutputStream fos = new FileOutputStream("Test2.txt");
+             String str = "hello this is me learning streams";
+                byte b[] = str.getBytes();
+
+             //   for(byte x : b)
+                    fos.write(b,5,str.length()-5);
+             
              fos.close();
         } catch (Exception e) {}
        
