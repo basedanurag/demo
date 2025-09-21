82e4a4fdb51aa5e7382f4026f321296ccc4c6bfd
basedanurag <anuragsrivastava241529@gmail.com>
2025-09-21 23:20:54 +0530

    CharArrayInputStreamDemo done

---
+++ JAVALEARNING/java io Streams/CharArrayInputStreamDemo.java
@@ -0,0 +1,15 @@
+import java.io.CharArrayReader;
+
+public class CharArrayInputStreamDemo {
+    // program for char array reader 
+    public static void main(String[] args)throws Exception {
+        char[] c = {'a','b', 'c', 'd'};
+        CharArrayReader car = new CharArrayReader(c);
+
+        int x ;
+        while( (x = car.read()) != -1){
+            System.out.print((char)x);
+        }
+        car.close();
+    }
+}
