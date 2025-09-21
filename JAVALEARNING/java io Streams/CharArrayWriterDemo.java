46f35632d7dba7f51d4a0dfabe5e90c9e3c6ad08
basedanurag <anuragsrivastava241529@gmail.com>
2025-09-21 23:27:52 +0530

    CharArrayWriterDemo done

--- JAVALEARNING/java io Streams/CharArrayWriterDemo.java
+++ JAVALEARNING/java io Streams/CharArrayWriterDemo.java
@@ -1,6 +1,16 @@
+import java.io.CharArrayWriter;
+
 public class CharArrayWriterDemo {
-    public static void main(String[] args) {
-        
-    
+    public static void main(String[] args) throws Exception{
+        char[] c = {'a','b', 'c', 'd'};
+    CharArrayWriter caw = new CharArrayWriter();
+   
+    caw.write(c);
+     caw.append(" ajhbasfhjga");
+        char[] c2  = caw.toCharArray();
+        for(char cc : c2){
+            System.out.print(cc);
+        }
+        caw.close();
     }
 }
