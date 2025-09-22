121e70f1e822664574177445ce194dddfc914a29
basedanurag <anuragsrivastava241529@gmail.com>
2025-09-22 22:39:17 +0530

    RandomAccessdemo done

---
+++ JAVALEARNING/java io Streams/RandomAccessdemo.java
@@ -0,0 +1,19 @@
+import java.io.RandomAccessFile;
+public class RandomAccessdemo {
+    public static void main(String[] args)throws Exception {
+        RandomAccessFile rf = new RandomAccessFile("Data.txt", "rw");
+        System.out.println((char)rf.read());
+        System.out.println((char)rf.read());
+        System.out.println((char)rf.read());
+        rf.write('d');
+        System.out.println((char)rf.read());
+        rf.skipBytes(1);
+        System.out.println((char)rf.read());
+        rf.seek(3);
+        System.out.println((char)rf.read());
+        System.out.println(rf.getFilePointer());
+        rf.seek(rf.getFilePointer()+2);
+        System.out.println((char)rf.read());
+
+    }
+}
