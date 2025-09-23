1f452ffd704e9bb388a27401cc2f93c0165346ed
basedanurag <anuragsrivastava241529@gmail.com>
2025-09-23 12:16:59 +0530

    DataInputOutputStreamsdemo done

--- JAVALEARNING/java io Streams/PrintStreamDemo.java
+++ JAVALEARNING/java io Streams/PrintStreamDemo.java
@@ -30,9 +30,9 @@ public class PrintStreamDemo {
         s.rollno = Integer.parseInt(br.readLine());
         s.name = br.readLine();
         s.dept = br.readLine();
-        System.out.println(s.rollno);
-       System.out.println(s.name);
-       System.out.println(s.dept);
+        System.out.println("roll number is "+s.rollno);
+       System.out.println("name is "+s.name);
+       System.out.println("Dept is "+s.dept);
        br.close();
 
     }
