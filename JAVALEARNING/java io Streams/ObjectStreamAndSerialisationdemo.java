ff1181a41ff4cccb8bb4bb27a1a955d03a45e717
basedanurag <anuragsrivastava241529@gmail.com>
2025-09-24 13:33:59 +0530

    done till object reading

---
+++ JAVALEARNING/java io Streams/ObjectStreamAndSerialisationdemo.java
@@ -0,0 +1,39 @@
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+class Student implements Serializable {
+    private int rollnum;
+    private String name;
+    private String dept;
+    static int data = 10;
+
+    public Student(){
+
+    }
+    public Student(int roll, String name, String dept){
+            this.rollnum = roll;
+            this.name = name;
+            this.dept = dept;
+            data = 5000;
+    }
+    public String toString(){
+        return "\n Student Details\n"+
+                "\n roll number :"+ rollnum+
+                "\n name :"+ name+
+                "\n department" + dept;
+    }
+}
+public class ObjectStreamAndSerialisationdemo {
+ public static void main(String[] args) throws Exception { 
+    /// code for writing the object in afile
+    /// 
+    FileOutputStream fos  = new FileOutputStream("Student.txt");
+    ObjectOutputStream oop = new ObjectOutputStream(fos); 
+
+    Student s = new Student(10, "Anurag", "CSE");
+    oop.writeObject(s);
+    oop.close();
+    fos.close();
+
+ }   
+}
