e7aff918dc20a7afdefa11c6e0693105b3c482a7
basedanurag <anuragsrivastava241529@gmail.com>
2025-10-04 02:12:40 +0530

    java generics basics little

---
+++ JAVALEARNING/java generics/GenericsDemo.java
@@ -0,0 +1,15 @@
+public class GenericsDemo<T> {
+    T data[] =(T[]) new Object[3];
+    public static void main(String[] args) {
+        GenericsDemo<String> gd = new GenericsDemo();
+        gd.data[0] = "Hello";
+        gd.data[1] = "how are you";
+        // gd.data[2] =  new Integer(19);
+      
+        String str = gd.data[0];
+        System.out.println(str);
+        str = gd.data[1];
+        System.out.println(str);
+
+    }
+}
