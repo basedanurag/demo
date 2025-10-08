ad9b293b0c28a666bd0d9bae9fd97295d44e400f
basedanurag <anuragsrivastava241529@gmail.com>
2025-10-08 16:34:08 +0530

    removing all the class files

--- JAVALEARNING/Collection FrameWork java/TreeSetDemo.java
+++ JAVALEARNING/Collection FrameWork java/TreeSetDemo.java
@@ -6,7 +6,7 @@ public class TreeSetDemo {
         TreeSet<Integer> ts = new TreeSet<>(List.of(10,20,30,40,90,60)); // as this implements sortedSet interface then all the elements will be sorted inside this 
         ts.add(10); // as this is a set so it donot have repeated values in it only uniqe elements are stored 
         ts.forEach(x ->  System.out.println(x));
-        System.out.println(ts.ceiling(19));// if the entered number is not present then  this method gives the greater closest number than element inside the HashSet;
+        System.out.println(ts.ceiling(19));// if the entered number is not present then  this method gives the greater closest number than element inside the treeSet;
         
     }
 }
