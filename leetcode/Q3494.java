9e2a07ff0334487b7e32cde9ea9651cbaf64bef7
basedanurag <anuragsrivastava241529@gmail.com>
2025-10-10 04:35:59 +0530

    this was hard to understand but i got this completely

---
+++ leetcode/Q3494.java
@@ -0,0 +1,23 @@
+public class Q3494 {
+    class Solution {
+    public long minTime(int[] skill, int[] mana) {
+        int n  =  skill.length;
+        int m  =  mana.length;
+        long[] finishTime =  new long[n];
+
+        for(int j  = 0 ; j < m; j++){
+            finishTime[0] +=  (long) (mana[j] * skill[0]);
+
+            for(int i = 1; i < n; i++){
+                finishTime[i] =  Math.max(finishTime[i] , finishTime[i - 1]) + (long)(mana[j] * skill[i]);
+            }
+            //for synchronization
+            for(int i = n-1; i > 0; --i){
+                finishTime[i - 1] = finishTime[i] - (mana[j] * skill[i]);
+            }
+        }
+    return finishTime[n - 1];
+
+    }
+}
+}
