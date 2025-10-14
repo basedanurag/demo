c357816a9a18455487880295b4e298bc5aaf85c6
basedanurag <anuragsrivastava241529@gmail.com>
2025-10-14 22:43:12 +0530

    done

--- JAVALEARNING/Collection FrameWork java/HashMapDemo.java
+++ JAVALEARNING/Collection FrameWork java/HashMapDemo.java
@@ -1,11 +1,14 @@
 import java.util.HashMap;
 
+
+
 public class HashMapDemo {
     public static void main(String[] args) {
         HashMap<Integer, String> mp = new HashMap<>();
         mp.put(1,"hello");
         mp.put(2, "kaise ho bhai");
         mp.put(3, "bye");
+        
 
         mp.forEach((k,v) -> System.out.println(k+ " " + v) );
     }
