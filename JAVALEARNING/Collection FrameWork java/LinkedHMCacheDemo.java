6bc099b39883da8a80653833e8e89987485cfc09
basedanurag <anuragsrivastava241529@gmail.com>
2025-10-19 01:33:07 +0530

    LEARNED HOW TO USE IT AS CACHE

--- JAVALEARNING/Collection FrameWork java/LinkedHMCacheDemo.java
+++ JAVALEARNING/Collection FrameWork java/LinkedHMCacheDemo.java
@@ -1,13 +1,22 @@
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class LinkedHMCacheDemo {
     public static void main(String[] args) {
-        LinkedHashMap<Integer, String> lmp =  new LinkedHashMap<>(5, .75f);
+        LinkedHashMap<Integer, String> lmp =  new LinkedHashMap<>(5, .75f,true){
+            protected boolean removeEldestEntry(Map.Entry e){
+                return size()>10;
+            }
+        };
         lmp.put(1,"Anurag");
         lmp.put(2,"Nishant");
         lmp.put(3,"neelam" );
         lmp.put(4,"Anurag");
-        // lmp.put(1,"Anjali"); // it will change the previous value and store the new value in this key bucket
+        lmp.put(6, "Aparna");
+        lmp.put(5,"kavishek");
+        lmp.put(7, "jigyashu");
+        lmp.put(8, "utkarsh");
+        // lmp.put(1,"Ansh"); // it will change the previous value and store the new value in this key bucket
 
         lmp.forEach((k,v) -> System.out.println(k + " "+ v));
     }
