18edbae0e15094056e2dbe417e90429f8275414f
basedanurag <anuragsrivastava241529@gmail.com>
2025-10-22 01:03:11 +0530

    StringTokenizerDemo done while reading from a file

--- JAVALEARNING/Collection FrameWork java/StringTokenizerDemo.java
+++ JAVALEARNING/Collection FrameWork java/StringTokenizerDemo.java
@@ -1,13 +1,20 @@
+import java.io.FileInputStream;
 import java.util.StringTokenizer;
 
 public class StringTokenizerDemo {
-    public static void main(String[] args) {
-        String data = "1=Anurag;2=Nishant;3=Manish;4=Neelam";
-        StringTokenizer st = new StringTokenizer(data, "=;");
+    public static void main(String[] args)throws Exception {
+        FileInputStream fis = new FileInputStream("Student.txt");
+        byte b[] = new byte[fis.available()];
+        fis.read(b);
+        String data = new String(b);
+
+
+        StringTokenizer st = new StringTokenizer(data, "=");
         String s ;
         while(st.hasMoreTokens()){
             s = st.nextToken();
             System.out.print(s + " ");
         }
+        fis.close();
     }
 }
