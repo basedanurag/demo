d536f487399d4e5feed26956e5936d538b722e01
basedanurag <anuragsrivastava241529@gmail.com>
2025-10-22 00:28:12 +0530

    practised PropertiesDemo done

---
+++ JAVALEARNING/Collection FrameWork java/PropertiesDemo.java
@@ -0,0 +1,19 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import java.util.Properties;
+
+public class PropertiesDemo {
+    //iske andar key and value both are strings
+    public static void main(String[] args) throws Exception {
+        Properties ps = new Properties();
+        // ps.setProperty("1","Hello" );
+        // ps.setProperty("2","How" );
+        // ps.setProperty("3","are" );
+        // ps.setProperty("4","you" );
+        // ps.storeToXML(new FileOutputStream("MyFileDemo.xml"), "My name is Anurag Srivasytava and this is my laptop of Lenov brand" );
+        ps.loadFromXML(new FileInputStream("MyFileDemo.xml"));
+        System.out.println(ps);
+        ps.forEach((k,v)-> System.out.println(k+" "+ v));
+    }
+}
