b25a9f2b7cbcf1f7c476580f9a178020230f158a
basedanurag <anuragsrivastava241529@gmail.com>
2025-10-24 17:31:04 +0530

    2 problems solved prinrName and printNumber using reccursion baki gym se aake krungaa

--- A2Z sheet solutions/ReccursionPlayList.java
+++ A2Z sheet solutions/ReccursionPlayList.java
@@ -8,7 +8,14 @@ public class ReccursionPlayList {
         System.out.println("Anurag");
         printName(n - 1);
     }
-   
+
+    //recursive method for printing 1 -> n linearly
+    public static void pintNumber(int i, int n){
+        if(i > n)
+        return;
+        System.out.print(i +" "); 
+        pintNumber(i + 1, n);
+    }
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
@@ -17,8 +24,12 @@ public class ReccursionPlayList {
         System.out.println("enter n"); 
         n = sc.nextInt();
 
-
+        System.out.println("output for printing name N times");
         printName(n);
+
+        System.out.println("output for printing 1 -> N (linearly) using reccursion");
+        pintNumber(1, n);
+    
 
 
 
