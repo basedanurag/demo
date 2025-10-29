80d2e914af192f1717952ac330076adde5f4d20b
basedanurag <anuragsrivastava241529@gmail.com>
2025-10-29 23:54:21 +0530

    BubbleSort done

---
+++ A2Z sheet solutions/Step 2/BubbleSort.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+        int [] arr = new int[5];
+        int n  = arr.length;
+        for (int i = 0; i < n; i++) {
+        System.out.print("enter the ith element of array to sort "+ i+ " ");
+            arr[i] = sc.nextInt();
+            System.out.println();
+        }
+
+        // BUBBLE SORT ALGORITHM
+            for(int i =  n - 1; i >=0; i--){
+                for (int j = 0; j <= i -1 ; j++) {
+                    if (arr[j]> arr[j + 1]){
+                        //swap
+                        int temp = arr[j+1];
+                        arr[j+1] = arr[j];
+                        arr[ j] = temp;
+                    }
+                }
+            }
+            System.out.println("array after sorting");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        sc.close();
+
+    }
+}
