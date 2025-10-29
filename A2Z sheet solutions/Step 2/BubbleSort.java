ba1a33c7af9a793d74a6f33085b7e340b3aac5d7
basedanurag <anuragsrivastava241529@gmail.com>
2025-10-29 23:58:23 +0530

    done

--- A2Z sheet solutions/Step 2/BubbleSort.java
+++ A2Z sheet solutions/Step 2/BubbleSort.java
@@ -14,13 +14,17 @@ public class BubbleSort {
 
         // BUBBLE SORT ALGORITHM
             for(int i =  n - 1; i >=0; i--){
+                int didSwap = 0;
                 for (int j = 0; j <= i -1 ; j++) {
                     if (arr[j]> arr[j + 1]){
                         //swap
                         int temp = arr[j+1];
                         arr[j+1] = arr[j];
                         arr[ j] = temp;
+                        didSwap = 1;
                     }
+                    if(didSwap == 0)
+                        break;
                 }
             }
             System.out.println("array after sorting");
