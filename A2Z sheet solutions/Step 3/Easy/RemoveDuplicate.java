91b5b2f2f552acf146a05f599965afc220bc7d5b
basedanurag <anuragsrivastava241529@gmail.com>
2025-11-09 17:36:12 +0530

    RemoveDuplicate bruteforce done

---
+++ A2Z sheet solutions/Step 3/Easy/RemoveDuplicate.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+public class RemoveDuplicate {
+    public static int  bruteFore(int[] arr){
+        LinkedHashSet<Integer> set = new LinkedHashSet<>(); // this take SC :- O(n)
+        //inserting the arrays element into the Set TC : O(nlogn)
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
+        }
+        //modifying array after insertion Tc : - O(n)
+        int index =0;
+        for (Integer it : set) {
+            arr[index] = it;
+            index++;
+        }   
+        return index;
+    }
+    public static void main(String[] args) {
+            int[] arr = {3,1,31,4,5,6,7,7,7,7,7,7,7,7};
+            Arrays.sort(arr);
+            int newLen = bruteFore(arr);
+            for (int i = 0; i < newLen; i++) {
+                System.out.print(arr[i]+" ");
+            }
+            
+            
+    }
+}
