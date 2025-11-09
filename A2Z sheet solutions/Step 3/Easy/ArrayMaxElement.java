1050be67e2f32751a598630a5e1a243c18b5daca
basedanurag <anuragsrivastava241529@gmail.com>
2025-11-09 05:37:20 +0530

    q1  completed by self

--- A2Z sheet solutions/Step 3/Easy/ArrayMaxElement.java
+++ A2Z sheet solutions/Step 3/Easy/ArrayMaxElement.java
@@ -1,11 +1,24 @@
-public class ArrayMaxElement {
-    public static void SortingMethod(){
+import java.util.Arrays;
 
+public class ArrayMaxElement {
+    public static int SortingMethod(int[] arr){
+        Arrays.sort(arr);
+        int Max = arr[arr.length - 1];
+        return Max;
     }
-    public static void reccursiveMethod(){
-        
+    public static int reccursiveMethod(int arr[],  int n){
+        int Max = arr[0];
+        for(int i = 0; i< n; i++){
+            if (arr[i] > Max){
+                Max = arr[i];
+            }
+        }
+        return Max;
     }
     public static void main(String[] args) {
-        
+        int[] arr = {3,1,31,4,5,6};
+        System.out.println("Using the sorting method to find the max element which is  "+SortingMethod(arr));
+
+        System.out.println("Using the Recursion method to find the max element which is  "+reccursiveMethod(arr,arr.length));
     }
 }
