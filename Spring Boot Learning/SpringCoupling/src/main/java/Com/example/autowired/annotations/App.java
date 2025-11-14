c4b2679836ea4a76da7753da9ce4ac4615866ff0
basedanurag <anuragsrivastava241529@gmail.com>
2025-11-14 10:20:50 +0530

    Learned about autowired and Qualifer annotations and their implementations

---
+++ Spring Boot Learning/SpringCoupling/src/main/java/Com/example/autowired/annotations/App.java
@@ -0,0 +1,4 @@
+package Com.example.autowired.annotations;
+
+public class App {
+}
---
+++ Spring Boot Learning/SpringCoupling/src/main/java/Com/example/autowired/annotations/AppConfig.java
@@ -0,0 +1,8 @@
+package Com.example.componentscan;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+@ComponentScan(basePackages = "Com.example.componentscan")
+public class AppConfig {
+}
