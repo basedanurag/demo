c76bfedc4e9475b61d74e67b8e7ed1a647b1ef04
basedanurag <anuragsrivastava241529@gmail.com>
2025-11-19 00:29:24 +0530

    solution 1 also done

--- A2Z sheet solutions/Step 3/Easy/UnionArray.java
+++ A2Z sheet solutions/Step 3/Easy/UnionArray.java
@@ -1,9 +1,27 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
 public class UnionArray {
-    public static List<Integer> solution1(int []arr1, int[] arr2){
+    public static void solution1(int[]arr1,int[]arr2){
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        ArrayList<Integer> union = new ArrayList<>();
+        for (int i = 0; i < arr1.length; i++) {
+            mp.put(arr1[i], mp.getOrDefault(arr1[i], 0) + 1);
+
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            mp.put(arr2[i],mp.getOrDefault(arr2[i], 0) + 1 );
+
+        }
+        for (int itr : mp.keySet()) {
+            union.add(itr);
+        }
+        printArr(union.stream().mapToInt(Integer::intValue).toArray());
+
+    }
+    public static List<Integer> solution2(int []arr1, int[] arr2){
          HashSet<Integer> set = new HashSet<>();
          List<Integer> union = new ArrayList<>();
          for(int i = 0; i < arr1.length; i++){
