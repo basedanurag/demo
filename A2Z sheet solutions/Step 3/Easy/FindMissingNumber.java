b8c230fdc24fd89d57f6ef491835c8eb165b4805
basedanurag <anuragsrivastava241529@gmail.com>
2025-12-05 22:50:04 +0530

    optimized approach done by my self

--- A2Z sheet solutions/Step 3/Easy/FindMissingNumber.java
+++ A2Z sheet solutions/Step 3/Easy/FindMissingNumber.java
@@ -1,12 +1,11 @@
 public class FindMissingNumber {
-    public static int MissingNumber1(int[] arr){
+    public static int MissingNumber1(int[] arr) {
         int num = 1;
         int result = -1;
         for (int i = 0; i < arr.length; i++) {
-            if(num == arr[i]){
+            if (num == arr[i]) {
                 num++;
-            }
-            else{
+            } else {
                 result = num;
             }
 
@@ -14,21 +13,29 @@ public class FindMissingNumber {
 
         return result;
     }
-    public static int MissingNumber2(int[]arr){
-        int result = -1;
-        int sum = 0;
-        int count= 0;
-        for (int i : arr) {
-            sum+=i;
-        }
-        
 
+    public static int MissingNumber2(int[] arr) {
+        int result = -1;
+        int sum1 = 0;
+        int n = arr[arr.length - 1];
+        int sum = 0;
+        // sum of number 1 - n
+        for (int i = 0; i < n; i++) {
+            sum1 += i;
+        }
+        // sum of whole array
+        for (int i : arr) {
+            sum += i;
+        }
+
+        result = sum - sum1;
         return result;
 
     }
+
     public static void main(String[] args) {
-        int[] arr = {1,2,4,5,6};
-        System.out.println("the missing number is:-"+MissingNumber1(arr));
+        int[] arr = { 1, 2, 4, 5, 6 };
+        System.out.println("the missing number is:-" + MissingNumber2(arr));
 
     }
 }
