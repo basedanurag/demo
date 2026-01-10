1a5091ff390d0579f3759e50b70a27f0000e565a
basedanurag <anuragsrivastava241529@gmail.com>
2026-01-11 03:08:31 +0530

    test

--- JAVALEARNING/MultiThreading/ThreadInterface.java
+++ JAVALEARNING/MultiThreading/ThreadInterface.java
@@ -2,19 +2,22 @@ public class ThreadInterface implements Runnable  {
     public void run(){
         int i = 1;
         while(true){
-         System.out.println(i+ "hello");
+         System.out.println(i+ " hello");
          i++;
         }
     }
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
         ThreadInterface t = new ThreadInterface();
         Thread th = new Thread(t);
         th.start();
+        t.wait(90);
+        
 
-        int i = 1;
-        while(true){
-         System.out.println(i+ "bye");
-         i++;
-        }
+
+        // int i = 1;
+        // while(true){
+        //  System.out.println(i+ "bye");
+        //  i++;
+        // }
     }
 }
