4daecbeb91ad70ffcae6012bff3379312520ce23
basedanurag <anuragsrivastava241529@gmail.com>
2026-01-18 01:09:37 +0530

    commit

--- A2Z sheet solutions/Step 3/Medium/TwoSum.java
+++ A2Z sheet solutions/Step 3/Medium/TwoSum.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class TwoSum{
     public static int[] TwoSumBrute(int[] arr, int n){
         // this is the most basic version of 2 pointer two sum TC (ON^2) and space is O(1)
@@ -10,6 +12,19 @@ public class TwoSum{
         }
 
         return null;
+    }
+    public static String TwoSumBetter(int[] arr, int n){
+        HashMap<Integer, Integer> hmp  = new HashMap<>();
+        for(int i = 0; i < arr.length;  i++){
+            int number = n - arr[i];
+            if(hmp.containsKey(number)){
+                return "Yes";
+            }
+
+            hmp.put(arr[i], i);
+        }
+        
+        return "NO";
     }
     public static void main(String[] args) {
         
