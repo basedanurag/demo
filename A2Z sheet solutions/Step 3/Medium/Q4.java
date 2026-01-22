0282c873bb76a9b92576c3a8d0ec5e8bb7564c4d
basedanurag <anuragsrivastava241529@gmail.com>
2026-01-22 08:53:10 +0530

    next is kadanes algorithm i will do that after nap

--- A2Z sheet solutions/Step 3/Medium/Q4.java
+++ A2Z sheet solutions/Step 3/Medium/Q4.java
@@ -14,7 +14,7 @@ public class Q4 {
         for (int i = 0; i < arr.length; i++) {  // select the starting point of sub array
             for (int j = i; j < arr.length; j++) { // select the ending point of a subarray
                      currSum= 0;
-                for (int k = i; k < j; k++) { // calculate the sum of the sub array 
+                for (int k = i; k <= j; k++) { // calculate the sum of the sub array 
                 currSum += arr[k];
                     MaxSum = Math.max(MaxSum, currSum);
 
@@ -24,11 +24,27 @@ public class Q4 {
         return MaxSum;
 
     }
+    public static int betterApproach(int[] arr){
+         int MaxSum =  Integer.MIN_VALUE;
+        int currSum = 0;
+        // this better approach   will work by adding new element to the everytime 
+        for (int i = 0; i < arr.length; i++) { 
+                currSum= 0;          // select the starting point of sub array
+            for (int j = i; j < arr.length; j++) { // select the ending point of a subarray
+                     
+                 // calculate the sum of the sub array 
+                currSum += arr[j];
+                    MaxSum = Math.max(MaxSum, currSum);
+            }
+        }
+        return MaxSum;
+    }
     public static void main(String[] args) {
          
         int [] arr = {2, 3, 5, -2, 7, -4};
         int[] nums = {-2, -3, -7, -2, -10, -4};
         System.out.println(MaxSum(nums));
+        System.out.println(betterApproach(arr));
         System.out.println(MaxSum(arr));
     }
 }
