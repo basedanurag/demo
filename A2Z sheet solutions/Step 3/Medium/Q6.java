27c4a5c805577d3d813b93b84e16d011133ec4a0
basedanurag <anuragsrivastava241529@gmail.com>
2026-01-25 06:34:08 +0530

    stock buy and sell brute force done

---
+++ A2Z sheet solutions/Step 3/Medium/Q6.java
@@ -0,0 +1,22 @@
+public class Q6 {
+    public static int bruteforce(int[] arr){
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0 ; i < arr.length; i++){
+
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[i] < arr[j]){
+                sum = arr[j] - arr[i];
+                max = Math.max(max, sum);
+                }
+
+            }
+            
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int arr[] = {7,1,5,3,6,4};
+        System.out.println(bruteforce(arr));
+    }
+}
