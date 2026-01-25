d6a68b330452d12524770c42d75fdbb83ec17373
basedanurag <anuragsrivastava241529@gmail.com>
2026-01-25 06:38:40 +0530

    some changes in code

--- A2Z sheet solutions/Step 3/Medium/Q6.java
+++ A2Z sheet solutions/Step 3/Medium/Q6.java
@@ -1,7 +1,7 @@
 public class Q6 {
     public static int bruteforce(int[] arr){
         int sum = 0;
-        int max = Integer.MIN_VALUE;
+        int max = 0; // cause selling on higher and buying on lower always give the +ve result
         for(int i = 0 ; i < arr.length; i++){
 
             for (int j = i+1; j < arr.length; j++) {
