3b907b7ff5739d64a97edf1d1ab5a32053c177ab
basedanurag <anuragsrivastava241529@gmail.com>
2026-01-25 07:53:47 +0530

    solved LC 121

--- A2Z sheet solutions/Step 3/Medium/Q6.java
+++ A2Z sheet solutions/Step 3/Medium/Q6.java
@@ -1,5 +1,64 @@
+import java.io.*;
+
+// public class Q6 {
+//     public static int bruteforce(int[] arr){
+//         int sum = 0;
+//         int max = 0; // cause selling on higher and buying on lower always give the +ve result
+//         for(int i = 0 ; i < arr.length; i++){
+
+//             for (int j = i+1; j < arr.length; j++) {
+//                 if(arr[i] < arr[j]){
+//                 sum = arr[j] - arr[i];
+//                 max = Math.max(max, sum);
+//                 }
+
+//             }
+            
+//         }
+//         return max;
+//     }
+//     public static void main(String[] args) throws Exception {
+//           FileInputStream fis = new FileInputStream("input.txt");
+//         System.setIn(fis);
+
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+//         sc.close();
+      
+//         System.out.println(bruteforce(arr));
+//     }
+// }
+
+
 public class Q6 {
-    public static int bruteforce(int[] arr){
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(
+                new FileReader("input.txt")
+        );
+
+        String line = br.readLine(); // entire test case
+        String[] parts = line.split(",");
+
+        int n = parts.length;
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(parts[i].trim());
+        }
+
+        System.out.println("Input size = " + n);
+
+        
+      //  System.out.println(solve(arr));
+         System.out.println(optimalDP(arr));
+    }
+
+    static int solve(int[] arr) {
         int sum = 0;
         int max = 0; // cause selling on higher and buying on lower always give the +ve result
         for(int i = 0 ; i < arr.length; i++){
@@ -15,8 +74,20 @@ public class Q6 {
         }
         return max;
     }
-    public static void main(String[] args) {
-        int arr[] = {7,1,5,3,6,4};
-        System.out.println(bruteforce(arr));
+    static int optimalDP(int[]arr){
+        int cost =0;
+        int min =  arr[0];
+        int profit = 0;
+        for (int i = 1; i < arr.length; i++) {
+            cost = arr[i] - min;
+            profit = Math.max(profit,cost);
+
+
+            // for tracing the minimum element i will track using mini and arr[i]
+            min = Math.min(min, arr[i]);
+
+        }
+        return profit;
     }
 }
+
