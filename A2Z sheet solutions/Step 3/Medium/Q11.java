daa8f7474f0176082592ff36ae5d4d41a7d52e73
basedanurag <anuragsrivastava241529@gmail.com>
2026-01-29 06:15:43 +0530

    bhai mere liye hard tha thoda but.... practice makes a man perfect brute force ka simulation part soch lia tha

---
+++ A2Z sheet solutions/Step 3/Medium/Q11.java
@@ -0,0 +1,48 @@
+public class Q11 {
+    // set matrix 2D array 
+    public static void printMat(int[][] matrix){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(" " + matrix[i][j]+ ",");
+            }
+            System.out.println();
+        }
+    }
+    public static void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(matrix[i][j] == 0){
+                    for (int k = 0; k < m; k++) {
+                        if(matrix[i][k] != 0){
+                            matrix[i][k] = -1;
+                        }
+                        
+                    }
+                    for (int row = 0; row < n; row++) {
+                        if(matrix[row][j] !=  0){
+                            matrix[row][j] = -1;
+                        }
+                    }
+                    
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < n; j++) {
+                if(matrix[i][j] == -1){
+                    matrix[i][j]= 0;
+                }
+            }
+        }
+
+        printMat(matrix);
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = {{1,1,1},{1,0,1},{1,1,1}};
+        setZeroes(matrix);
+    }
+}
