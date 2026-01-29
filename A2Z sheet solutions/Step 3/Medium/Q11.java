a08c388d7cad02fa572dbf5ba624a2be68a27ad8
basedanurag <anuragsrivastava241529@gmail.com>
2026-01-29 06:55:52 +0530

    better approach bhi solve hogya isme 2 row me track krna tha

--- A2Z sheet solutions/Step 3/Medium/Q11.java
+++ A2Z sheet solutions/Step 3/Medium/Q11.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
+
 public class Q11 {
     // set matrix 2D array 
+    public static void printArr(boolean arr[]){
+        for (boolean i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
     public static void printMat(int[][] matrix){
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
@@ -15,13 +23,13 @@ public class Q11 {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if(matrix[i][j] == 0){
-                    for (int k = 0; k < m; k++) {
+                    for (int k = 0; k < n; k++) {
                         if(matrix[i][k] != 0){
                             matrix[i][k] = -1;
                         }
                         
                     }
-                    for (int row = 0; row < n; row++) {
+                    for (int row = 0; row < m; row++) {
                         if(matrix[row][j] !=  0){
                             matrix[row][j] = -1;
                         }
@@ -41,9 +49,37 @@ public class Q11 {
 
         printMat(matrix);
     }
+    public static void betterApproach(int[][] matrix){
+        //TC O(N^2) sc : O(n).
+        int m  =  matrix.length;
+        int n =  matrix[0].length;
+        boolean [] row  = new boolean[m];
+        boolean [] col =  new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j <n; j++) {
+                if(matrix[i][j] == 0){
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        
+        }   
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(row[i] || col[j]){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        printMat(matrix);
+        printArr(row);
+        printArr(col);
+    }
 
     public static void main(String[] args) {
         int matrix[][] = {{1,1,1},{1,0,1},{1,1,1}};
-        setZeroes(matrix);
+       // setZeroes(matrix);
+        System.out.println();
+        betterApproach(matrix);
     }
 }
