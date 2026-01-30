d0036a12613af1cea2d718cd23777385227042bc
basedanurag <anuragsrivastava241529@gmail.com>
2026-01-30 11:02:09 +0530

    done

--- A2Z sheet solutions/Step 3/Medium/Q12.java
+++ A2Z sheet solutions/Step 3/Medium/Q12.java
@@ -10,6 +10,31 @@ public class Q12 {
         }
         printMat(ans);
     }
+    public static void optimalSol(int[][] arr){
+        int m = arr.length;
+        int n = arr[0].length;
+         for (int i = 0; i < m; i++) {
+            for (int j = i + 1 ; j < n; j++) {
+                int temp =  arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+         for(int i = 0; i < m; i++){
+            int low = 0;
+            int high = m -1;    
+
+            while(low < high){
+                int temp = arr[i][low];
+                arr[i][low] = arr[i][high];
+                arr[i][high] = temp;
+                low++;
+                high--;
+            }
+
+        }
+        printMat(arr);
+    }
      public static void printMat(int[][] matrix){
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
@@ -19,7 +44,9 @@ public class Q12 {
         }
     }
     public static void main(String[] args) {
-        int[][] ans = {{1, 2, 3},{4, 5, 6}, {7, 8, 9}};
-        bruteForce(ans);
+        int[][] ans = {{1, 2, 3},{4, 5, 6}, {7, 8, 9}}; // m must be equal to n 
+       // bruteForce(ans);
+        System.out.println();
+        optimalSol(ans);
     }
 }
