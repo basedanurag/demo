791802f36d132b6669d3f41460da0abdfe6dd027
basedanurag <anuragsrivastava241529@gmail.com>
2026-01-31 11:03:45 +0530

    done

---
+++ A2Z sheet solutions/Step 3/Medium/Q13.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Q13 {
+    public static void spiralMat(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        List<Integer> ans = new ArrayList<>();
+        int top = 0;
+        int bottom = n - 1;
+        int left = 0;
+        int right = m - 1;
+        while (top <= bottom && left <= right) {
+            // left to right
+            for (int i = left; i <= right; i++) {
+                ans.add(matrix[top][i]);
+            }
+            top++;
+            // right to bottom
+            for (int i = top; i <= bottom; i++) {
+                ans.add(matrix[i][right]);
+            }
+            right--;
+            // right to left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.add(matrix[bottom][i]);
+                }
+
+                bottom--;
+            }
+            // bottom to top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        for (int i : ans) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] ans = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        spiralMat(ans);
+    }
+}
