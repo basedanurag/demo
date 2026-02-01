d4a595060633f7c73070c9767cd40d229e223439
basedanurag <anuragsrivastava241529@gmail.com>
2026-02-01 23:38:50 +0530

    done

--- A2Z sheet solutions/Step 3/Medium/Q14.java
+++ A2Z sheet solutions/Step 3/Medium/Q14.java
@@ -1,6 +1,6 @@
 public class Q14 {
     public static int bruteforce(int[]arr, int n){
-        int  count =0;
+        int  count =0; //TC O(n^3)
         for(int i = 0; i < arr.length; i++){
             for (int j = i; j < arr.length; j++) {
                 
@@ -19,8 +19,23 @@ public class Q14 {
         }
         return count;
     }
+    public static int betterapproach(int[]arr, int k){
+        int count= 0;
+        int sum  = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum =0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+
+                if(sum == k)count++;
+            }
+        }
+        
+        return count;
+    }
     public static void main(String[] args) {
         int[] arr = {3, 1, 2, 4};
        System.out.println( bruteforce(arr, 6));
+       System.out.println(betterapproach(arr, 6));
     }
 }
