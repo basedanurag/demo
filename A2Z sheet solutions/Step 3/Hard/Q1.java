883c7c2bdceaaa89327e4cbcbdbb22864fd777b1
basedanurag <anuragsrivastava241529@gmail.com>
2026-02-04 20:27:52 +0530

    coded 1 better approach here for printing Nth row

--- A2Z sheet solutions/Step 3/Hard/Q1.java
+++ A2Z sheet solutions/Step 3/Hard/Q1.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Q1{
     public static void printarr(int[] arr){
         for (int i : arr) {
@@ -6,7 +9,15 @@ public class Q1{
     }
     // extreme brute force approach here 
                                     // isme n -1 and r - 1 value pass karni hai tabhi sahi answer ayega 
-    public static int pascalsValue(int n , int r){
+    public static List<List<Integer>> generate(int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 1; i < n; i++){
+          //  ans.add( printingNthRow(i+1));
+          ans.add(printNthRowBetter(i));
+        }
+        return ans;
+    }
+     public static int pascalsValue(int n , int r){
         int res = 1;
         for (int i = 0; i < r ; i++) { 
             res = res * (n - i);
@@ -14,17 +25,33 @@ public class Q1{
         }
         return res;
     }
-    public static void printingNthRow(int n){
-        int arr[] = new int[n];
+    public static List<Integer> printingNthRow(int n){
+        List<Integer> ans= new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
-           arr[i]  = pascalsValue(n - 1,(i + 1 ) - 1);
+           ans.add( pascalsValue(n - 1,(i + 1 ) - 1));
         }
-        printarr(arr);
+        return ans;
+    }
+
+    // better approach
+    public static List<Integer> printNthRowBetter(int n){
+        List<Integer> ans =  new ArrayList<>();
+        int res = 1;
+        ans.add(res);
+        for (int i = 1; i < n; i++) {
+            res = res *(n - i);
+            res = res / i;
+            ans.add(res);
+        }
+        return ans;
 
     }
     public static void main(String[] args) {
-                                        //example hai ye extreme brute force ka 
-        System.out.println(pascalsValue(5- 1,  3 - 1));
-        printingNthRow(5);
+      
+     System.out.println(generate(5)); // extreme brute force soln Tc O(n^3)
+
+
+    
     }
 }
