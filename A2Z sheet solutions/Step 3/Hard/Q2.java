768335d32a4b310896b07f0f39a8f0d2b182176e
basedanurag <anuragsrivastava241529@gmail.com>
2026-02-06 20:24:42 +0530

    added code for the optimal version of majority element 2 and added Q3 java file code will push later

--- A2Z sheet solutions/Step 3/Hard/Q2.java
+++ A2Z sheet solutions/Step 3/Hard/Q2.java
@@ -14,7 +14,52 @@ public class Q2 {
         }
         return ans;
     }
-    public static void main(String[] args) {
+     public static  List<Integer> majorityElement(int[] arr) {
+        List<Integer> ans = new ArrayList<>();
+        int n  = arr.length;
+        int element1 = 0;
+        int count1 = 0;
+        int element2 = 0;
+        int count2 = 0;
+        for(int i : arr){
+        if(i == element1){ 
+            count1++;
+        }
+        else if(i == element2){ 
+            count2++;
         
+        }
+        else if(count1 == 0){
+            element1 = i;
+            count1+=1;
+        }
+        else if(count2 == 0){
+            element2 = i;
+            count2+= 1;
+        }
+
+        else{
+            count1--;
+            count2--;
+        }
+
+      }
+        // verification
+        count1 = 0;
+        count2 = 0;
+         for (int num : arr) {
+        if (num == element1) count1++;
+        else if (num == element2) count2++;
+         }
+        if (count1 > n / 3) ans.add(element1);
+        if (count2 > n / 3) ans.add(element2);
+        
+      return ans;
+    }
+
+    public static void main(String[] args) {
+        int arr [] = {1, 2, 1, 1, 3, 2, 2,1,1,1,1,1,2,3,3,3,3,3,3,3,3};
+        System.out.println(freq(arr));
+        System.out.println(majorityElement(arr));
     }
 }
