6d34eb5cd0b593b947447b380a6a693193a1f586
basedanurag <anuragsrivastava241529@gmail.com>
2026-02-08 00:30:53 +0530

    bruteforce for 3 sum done by self using O(n^3) and O(n); space

--- A2Z sheet solutions/Step 3/Hard/Q3.java
+++ A2Z sheet solutions/Step 3/Hard/Q3.java
@@ -1,3 +1,33 @@
+import java.util.*;
+
 public class Q3 {
-    
+    public static List<List<Integer>> bruteforce(int[] arr){
+       // List<List<Integer>> res  = new ArrayList<>();
+        Set<List<Integer>> set = new HashSet<>();
+        int n = arr.length;
+        if(n < 3){
+            return new ArrayList<>(set);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                for (int j2 = j + 1; j2 < arr.length; j2++) { 
+                    if (arr[i] + arr[j] + arr[j2] == 0){
+                        List<Integer> ans= Arrays.asList(arr[i],arr[j],arr[j2]);
+                        Collections.sort(ans);
+                        set.add(ans);
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        return new ArrayList<>(set);
+    }
+    public static void main(String[] args) {
+        int [] arr = {-1,0,1,2,-1,-4};
+                    // 0,1,2,3, 4 ,5
+        System.out.println(bruteforce(arr));
+    }
 }
