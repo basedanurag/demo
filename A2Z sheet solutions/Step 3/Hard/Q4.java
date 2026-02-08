ce6edbdef3b6f0f68ed25f46f33fe42fa48e157c
basedanurag <anuragsrivastava241529@gmail.com>
2026-02-09 04:15:39 +0530

    coded the better approach by reffering to Q3 3 sum problem

--- A2Z sheet solutions/Step 3/Hard/Q4.java
+++ A2Z sheet solutions/Step 3/Hard/Q4.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class Q4 {
     public static List<List<Integer>> bruteforce(int[] arr, int target){
-        int sum  = 0;
         Set<List<Integer>> st = new HashSet<>();
         for (int i = 0; i < arr.length; i++) {
 
@@ -24,6 +23,30 @@ public class Q4 {
             }
         }
         return new ArrayList<>(st);
+    }
+    public static List<List<Integer>> betterAprroach(int[]arr, int target){
+    
+            Set<List<Integer>> set = new HashSet<>();
+            for (int i = 0; i < arr.length; i++) {
+               
+                for (int j = i + 1; j < arr.length; j++) {
+                     Set<Integer> set2 = new HashSet<>();
+                     
+                    for (int j2 = j + 1; j2 < arr.length; j2++) {
+                        int fourth  = target - (arr[i] + arr[j] + arr[j2]);
+                        
+                        if(set2.contains(fourth)){
+                            List<Integer> ans = Arrays.asList(arr[i], arr[j], arr[j2], fourth);
+                            Collections.sort(ans);
+                            set.add(ans);
+
+                        }
+                    set2.add(arr[j2]);
+                }
+                
+            }
+        }
+        return new ArrayList<>(set);
     }
     public static List<List<Integer>> optimalApproach(int[]arr, int target){
         int n = arr.length;
@@ -65,5 +88,6 @@ public class Q4 {
         int[] arr = {1,0,-1,0,-2,2};
         System.out.println(optimalApproach(arr, 0));
         System.out.println(bruteforce(arr, 0));
+        System.out.println(betterAprroach(arr, 0));
     }
 }
