9973d8236a147117e37b85717b9df3d8ff9cc898
basedanurag <anuragsrivastava241529@gmail.com>
2026-03-02 20:17:45 +0530

    coded the optimal approach as well

--- A2Z sheet solutions/Step 3/Hard/Q9.java
+++ A2Z sheet solutions/Step 3/Hard/Q9.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 public class Q9{
+    //this approach is invalid cause we cannot modify the array but we can create a copy and then modify it which will work but will not be so optimal
     public static int[] mythinking(int[]arr){
         int first = 0;
         int last = first + 1;
@@ -52,12 +53,34 @@ public class Q9{
         
         return ans;
     }
-    
+    public static int[] optimalAprroach(int[] arr){
+        int n  = arr.length;
+        long s = 0, s2= 0;
+        long SN = (n * (n + 1))/2;
+        long S2N = (n * (n + 1) * (2*n+ 1))/6;
+        for(int i = 0; i < n; i++){
+            s += arr[i];
+            s2 +=  (long)arr[i] *(long) arr[i];
+        }
+        long val1 = s - SN; 
+        long val2 = s2 - S2N;
+        val2 = val2 / val1;
+        long x = (val1 + val2) /2;
+        long y =  x - val1;
+        int[] ans =  {(int) x, (int)y};
+        for (int j : ans) {
+        System.out.print(j+" ");
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 6, 7, 5, 7};  //{1,2,3,5,6,7,7}
                                             //{1,2,3,4,5,6,7}
         mythinking(arr);
         System.out.println();
         verynaive(arr);
+        System.out.println();
+        optimalAprroach(arr);
+
     }
 }
