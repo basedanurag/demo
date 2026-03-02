1400671ec909710803e5be6ea9dbc85634e228e5
basedanurag <anuragsrivastava241529@gmail.com>
2026-03-02 19:03:47 +0530

    very naive approach done moving towards a better approach

--- A2Z sheet solutions/Step 3/Hard/Q9.java
+++ A2Z sheet solutions/Step 3/Hard/Q9.java
@@ -21,8 +21,43 @@ public class Q9{
         }
         return ans;
     }
+    public static int[] verynaive(int[] arr){
+        int missing = -1;
+        int repeating = -1;
+        int n  = arr.length;
+        for (int i = 1; i <= arr.length; i++) {
+            int count  = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if( arr[j]== i){
+                    count++;
+                }
+            }
+                if(count == 2){
+                    repeating = i;
+                }
+                else if(count == 0){
+                    missing =  i;
+                }
+
+            if(missing != -1 && repeating != -1){
+                break;
+            }
+            
+            
+        }
+        int[] ans = {repeating,missing};
+        for (int i : ans) {
+            System.out.print(i+" ");
+        }
+        
+        return ans;
+    }
+    
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 6, 7, 5, 7}; // {1,2,3,5,6,7,7}
+        int arr[] = {1, 2, 3, 6, 7, 5, 7};  //{1,2,3,5,6,7,7}
+                                            //{1,2,3,4,5,6,7}
         mythinking(arr);
+        System.out.println();
+        verynaive(arr);
     }
 }
