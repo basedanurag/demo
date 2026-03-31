0713d5e99113f774996b757af533d51f23ef80fb
basedanurag <anuragsrivastava241529@gmail.com>
2026-04-01 00:29:59 +0530

    bruteforce done

--- A2Z sheet solutions/Step 4/BS on ans/Q1.java
+++ A2Z sheet solutions/Step 4/BS on ans/Q1.java
@@ -1,5 +1,27 @@
 public class Q1{
+    public static int bruteforce(int num) {
+        
+        for (int i = 0; i < num; i++) {
+            int root  = 0;
+             root = i * i;
+            if(num == 1){
+                return num;
+            }
+            if(root == num){
+                return i;
+            }
+            if(root > num){
+                return i - 1;
+            }
+            
+        }
+        return 0;
+    }   
+    public static int optimal(int num){
+        
+    }
     public static void main(String[] args) {
+        System.out.println(bruteforce(16));
         System.out.println("hello world");
     }
 }
