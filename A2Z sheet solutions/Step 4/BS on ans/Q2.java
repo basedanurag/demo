66a81699b018d6b88aed660b74aad83d9ca341f4
basedanurag <anuragsrivastava241529@gmail.com>
2026-04-03 16:03:05 +0530

    brute force also done

--- A2Z sheet solutions/Step 4/BS on ans/Q2.java
+++ A2Z sheet solutions/Step 4/BS on ans/Q2.java
@@ -1,7 +1,16 @@
 public class Q2 {
-    // public static int bruteforce(int n , int m){
-       
-    // }
+    public static int bruteforce(int n , int m){
+       int ans = -1;
+       for (int i = 1; i < m; i++) {
+            ans = (int ) Math.pow(i,n);
+
+            if(ans == m) return i;
+
+            if (ans > m) break;
+
+       }
+       return -1;
+    }
     public static int optimal(int n , int m){
         int low = 1;
         int high = m/2;
@@ -30,6 +39,10 @@ public class Q2 {
         return ans;
     }
     public static void main(String[] args) {
+        System.out.println(bruteforce(3, 27));
+        System.out.println(bruteforce(4, 69));
+        System.out.println(bruteforce(2, 16));
+        System.out.println();
         System.out.println(optimal(3, 27));
         System.out.println(optimal(4, 69));
         System.out.println(optimal(2, 16));
