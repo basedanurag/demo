dae8f20ac97d1bea66fd662949b62105a0644c45
basedanurag <anuragsrivastava241529@gmail.com>
2026-04-17 09:40:15 +0530

    BS done by self

--- A2Z sheet solutions/Step 4/BS on ans/Q8.java
+++ A2Z sheet solutions/Step 4/BS on ans/Q8.java
@@ -29,8 +29,35 @@ public class Q8{
         }
         return ans;
     }
+    public static int optimal(int arr[], int cows){
+        // Arrays.sort(arr);
+        // int min  =  arr[0];
+        // int max = arr[arr.length - 1];
+       int max = Arrays.stream(arr).max().getAsInt();
+       int min =  Arrays.stream(arr).min().getAsInt();
+       int low =  1;
+       int maxDistance =  max - min;
+       int ans  = 0;
+
+
+       while (low <= maxDistance) {
+        int mid =  (maxDistance + low)/2;
+
+        if(checkPossible(arr, mid, cows)){
+            ans  = mid;
+            low =  mid + 1;
+        }else{
+            maxDistance =  mid - 1;
+        }
+        
+       }
+       return ans;
+
+    }
     public static void main(String[] args) {
-       int cows = 2, arr[] = {4,2,1,3,6};
+    //    int cows = 2, arr[] = {4,2,1,3,6};
+      int cows = 4, arr[] = {0,3,4,7,10,9}; 
         System.out.println(bruteForce(arr, cows));
+        System.out.println(optimal(arr, cows));
     }
 }
