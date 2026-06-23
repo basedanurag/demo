62482a0a07d3fef15e3573fe5772b2117fd43ab2
basedanurag <anuragsrivastava241529@gmail.com>
2026-06-24 00:50:09 +0530

    done

---
+++ A2Z sheet solutions/Step 6/Problems on LL/Q2.java
@@ -0,0 +1,33 @@
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+public class Q2 {
+    /**
+ * Definition for singly-linked list.
+  class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev =  null;
+        ListNode curr = head;
+        while(curr != null){
+            ListNode nextNode = curr.next;
+            curr.next =  prev;
+            prev = curr;
+            curr = nextNode;
+
+        }
+        return prev;
+    }
+}
+}
