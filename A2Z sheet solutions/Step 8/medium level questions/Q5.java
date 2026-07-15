1eef0d47d81cba97b5b328d3568a637b5a8f0c75
basedanurag <anuragsrivastava241529@gmail.com>
2026-07-15 12:08:59 +0530

    medium problem done

--- A2Z sheet solutions/Step 8/medium level questions/Q5.java
+++ A2Z sheet solutions/Step 8/medium level questions/Q5.java
@@ -1,4 +1,23 @@
 public class Q5 {
+    public int[] singleNumber(int[] nums) {
+        int num = 0;
+        for(int i : nums){
+            num ^= i;
+        }
+        int rightMost = (num & (num - 1) ^ num);
+
+        int b1 = 0;
+        int b2 = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            if((nums[i] & rightMost) != 0){
+                b1^= nums[i];
+            }else{
+                b2^= nums[i];
+            }
+        }
+        return new int[] {b1, b2};
+    }
     public static void main(String[] args) {
         
     }
