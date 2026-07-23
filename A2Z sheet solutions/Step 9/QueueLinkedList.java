b8e2546b53d519a73a9d7ebda6e750c757401801
basedanurag <anuragsrivastava241529@gmail.com>
2026-07-23 21:20:51 +0530

    ake karta

---
+++ A2Z sheet solutions/Step 9/QueueLinkedList.java
@@ -0,0 +1,12 @@
+class Node{
+    int val;
+    Node next;
+
+    public Node(int val){
+        this.val = val;
+        this.next = null;
+    }
+}
+public class QueueLinkedList {
+    
+}
--- A2Z sheet solutions/Step 9/StackLinkedList.java
+++ A2Z sheet solutions/Step 9/StackLinkedList.java
@@ -11,7 +11,21 @@ public class StackLinkedList {
         static Node top;
         static  int size = 0;
     public static void push(int x){
-        
+        Node temp =  new Node(x);
+        temp.next = top;
+        top = temp;
+        size +=1;
+    }
+    public static void pop(){
+        Node temp = top;
+        top = top.next;
+        size -=1;
+    }
+    public static int top(){
+        return top.val;
+    }
+    public static int size(){
+        return size;
     }
     public static void main(String[] args) {
         
