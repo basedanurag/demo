f1e5de6b081cfc61736e6187f24ae482edee0080
basedanurag <anuragsrivastava241529@gmail.com>
2026-07-24 09:18:03 +0530

    stack using queue

--- A2Z sheet solutions/Step 9/QueueLinkedList.java
+++ A2Z sheet solutions/Step 9/QueueLinkedList.java
@@ -8,5 +8,45 @@ class Node{
     }
 }
 public class QueueLinkedList {
-    
+    static Node top;
+    static Node start , end  = null;
+    static int size = 0;
+    public static void push(int z){
+        Node temp =  new Node(z);
+        if(start == null ){
+            start = end = temp;
+        }
+        else{
+        end.next= temp;
+        end =  temp;
+        }
+
+        size ++;
+    }
+    public static void pop(){
+        if(start == null){
+            return;
+        }
+        start =  start.next;
+        size -=1;
+    }
+    public static void peek(){
+        if(start == null) return ;
+
+       System.out.println(start.val +" ");
+    }
+    static int size(){
+        return size;
+    }
+
+    public static void main(String[] args) {
+        push(7);
+        push(2);
+        push(9);
+        peek();
+
+        pop();
+        peek();
+    }
+   
 }
