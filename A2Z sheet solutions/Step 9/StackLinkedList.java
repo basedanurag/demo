07cee2ecdfdf04f7456234e352b063a1f0b2d1ec
basedanurag <anuragsrivastava241529@gmail.com>
2026-07-24 08:38:05 +0530

    stackLinkedlist completed

--- A2Z sheet solutions/Step 9/StackLinkedList.java
+++ A2Z sheet solutions/Step 9/StackLinkedList.java
@@ -1,33 +1,67 @@
-class Node{
+class Node {
     int val;
     Node next;
 
-    public Node(int val){
+    Node(int val) {
         this.val = val;
-        this.next = null;
     }
 }
+
 public class StackLinkedList {
-        static Node top;
-        static  int size = 0;
-    public static void push(int x){
-        Node temp =  new Node(x);
+
+    static Node top;
+    static int size = 0;
+
+    public static void push(int x) {
+        Node temp = new Node(x);
         temp.next = top;
         top = temp;
-        size +=1;
+        size++;
     }
-    public static void pop(){
-        Node temp = top;
+
+    public static void pop() {
+        if (top == null) {
+            System.out.println("Stack Underflow");
+            return;
+        }
+
         top = top.next;
-        size -=1;
+        size--;
     }
-    public static int top(){
+
+    public static int peek() {
+        if (top == null)
+            throw new RuntimeException("Stack is empty");
+
         return top.val;
     }
-    public static int size(){
-        return size;
+
+    public static void display() {
+        Node temp = top;
+
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+
+        System.out.println();
     }
+
     public static void main(String[] args) {
-        
+
+        push(5);
+        push(2);
+        push(4);
+        push(1);
+        push(6);
+
+        display();      // 6 1 4 2 5
+
+        pop();
+
+        display();      // 1 4 2 5
+
+        System.out.println(peek()); // 1
+        System.out.println(size);   // 4
     }
 }
